19dc85380041d76844c82b24412b511adf2525e4
sentientwaffle <sentientwaffle@gmail.com>
2021-05-26 14:22:49 -0700

    fix(connector): formatting issues?

--- packages/connector/src/services/core/errors/index.ts
+++ packages/connector/src/services/core/errors/index.ts
@@ -2,9 +2,7 @@ export * from './invalid-json-body-error'
 
 export class AccountNotFoundError extends Error {
   constructor(accountId: string) {
-    super(
-      'Account not found. accountId=' + accountId
-    )
+    super('Account not found. accountId=' + accountId)
     this.name = 'AccountNotFoundError'
   }
 }
