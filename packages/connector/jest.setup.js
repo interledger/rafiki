0d21d0348df42eb073ae23a214c217f35d206599
sentientwaffle <sentientwaffle@gmail.com>
2021-06-03 07:48:42 -0700

    chore(connector): fail tests if redis is missing

--- packages/connector/jest.setup.js
+++ packages/connector/jest.setup.js
@@ -1,8 +1,11 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const Knex = require('knex')
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const IORedis = require('ioredis')
 const DATABASE_URL =
   process.env.DATABASE_URL ||
   'postgresql://postgres:password@localhost:5432/testing'
+const redis = new IORedis('redis://127.0.0.1:6379')
 
 module.exports = async () => {
   const knex = Knex({
@@ -23,4 +26,10 @@ module.exports = async () => {
     directory: __dirname + '/migrations'
   })
   global.__KNEX__ = knex
+
+  if (redis.status === 'ready') {
+    await redis.disconnect()
+  } else {
+    throw new Error('expected redis at 127.0.0.1:6379')
+  }
 }
