bba19be8d091ec90442a120a4021fe005765b48d
djg <sentientwaffle@gmail.com>
2021-06-18 07:35:51 -0700

    feat(backend): spsp endpoint (#20)
    - Add the SPSP endpoint to the backend's public http server (`/pay/:id`).
    - The tests pass, but this is incomplete:
      - The `id` in `/pay/:id` is currently the ILP account id, but it should be the id of the user within the backend.

--- packages/backend/src/config/app.ts
+++ packages/backend/src/config/app.ts
@@ -1,3 +1,5 @@
+import * as crypto from 'crypto'
+
 function envString(name: string, value: string): string {
   const envValue = process.env[name]
   return envValue == null ? value : envValue
@@ -16,7 +18,7 @@ function envInt(name: string, value: number): number {
 export const Config = {
   logLevel: envString('LOG_LEVEL', 'info'),
   port: envInt('PORT', 3001),
-  adminPort: envInt('ADMIN_PORT', 3002),
+  adminPort: envInt('ADMIN_PORT', 3003),
   databaseUrl: envString(
     'DATABASE_URL',
     'postgresql://postgres:password@localhost:5432/development'
@@ -25,6 +27,11 @@ export const Config = {
   redisUrl: envString('REDIS_URL', 'redis://127.0.0.1:6379'),
   coilApiGrpcUrl: envString('COIL_API_GRPC_URL', 'localhost:6000'),
   nonceRedisKey: envString('NONCE_REDIS_KEY', 'nonceToProject'),
+
+  ilpAddress: envString('ILP_ADDRESS', 'test.rafiki'),
+  streamSecret: process.env.STREAM_SECRET
+    ? Buffer.from(process.env.STREAM_SECRET, 'base64')
+    : crypto.randomBytes(32),
 
   /** Frontend **/
   frontendUrl: envString('FRONTEND_URL', 'http://localhost:3000')
