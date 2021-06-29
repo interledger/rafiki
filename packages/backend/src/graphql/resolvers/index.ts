123c3ea6d2bfe29ff27272e9af98baba54fbfd61
Cairin Michie <cairinmichie@gmail.com>
2021-06-29 12:17:20 +0200

    feat: stub invoice resolvers

--- packages/backend/src/graphql/resolvers/index.ts
+++ packages/backend/src/graphql/resolvers/index.ts
@@ -1,14 +1,32 @@
 import { Resolvers } from '../generated/graphql'
 import { getUser, getAccount, getBalance } from './user'
+import {
+  getUserInvoices,
+  getInvoiceEdges,
+  getPageInfo,
+  getInvoiceEdgeNode,
+  getInvoiceItems
+} from './invoice'
 
 export const resolvers: Resolvers = {
   Query: {
     user: getUser
   },
   User: {
-    account: getAccount
+    account: getAccount,
+    invoices: getUserInvoices
   },
   Account: {
     balance: getBalance
+  },
+  InvoiceConnection: {
+    edges: getInvoiceEdges,
+    pageInfo: getPageInfo
+  },
+  InvoiceEdge: {
+    node: getInvoiceEdgeNode
+  },
+  Invoice: {
+    items: getInvoiceItems
   }
 }
---
+++ packages/backend/src/graphql/resolvers/invoice.ts
@@ -0,0 +1,64 @@
+import {
+  ResolversTypes,
+  InvoiceConnectionResolvers,
+  UserResolvers,
+  InvoiceEdgeResolvers,
+  InvoiceResolvers
+} from '../generated/graphql'
+
+export const getUserInvoices: UserResolvers['invoices'] = async (
+  parent,
+  args,
+  ctx
+): ResolversTypes['InvoiceConnection'] => {
+  const logger = ctx.container.use('logger')
+  logger.info(args, 'getUserInvoices args')
+  // TODO: edges: generate node ids for edges.
+  // TODO: PageInfo: generate partial PageInfo that can be derived from edges.
+  return {}
+}
+
+export const getPageInfo: InvoiceConnectionResolvers['pageInfo'] = async (
+  parent,
+  args,
+  ctx
+): ResolversTypes['PageInfo'] => {
+  const logger = ctx.container.use('logger')
+  logger.info(args, 'getPageInfo args')
+  // TODO: generate missing PageInfo that can't be derived from edges.
+  return {}
+}
+
+export const getInvoiceEdges: InvoiceConnectionResolvers['edges'] = async (
+  parent,
+  args,
+  ctx
+): Promise<ResolversTypes['InvoiceEdge'][]> => {
+  const logger = ctx.container.use('logger')
+  logger.info(args, 'getInvoiceEdges args')
+  // TODO: generate array of cursors that match edge ids in parent.
+  return []
+}
+
+export const getInvoiceEdgeNode: InvoiceEdgeResolvers['node'] = async (
+  parent,
+  args,
+  ctx
+): ResolversTypes['Invoice'] => {
+  const logger = ctx.container.use('logger')
+  logger.info(args, 'getInvoiceEdgeNode args')
+  // TODO: return invoice id
+  // TODO: totalAmount: calculate total for invoice.
+  return {}
+}
+
+export const getInvoiceItems: InvoiceResolvers['items'] = async (
+  parent,
+  args,
+  ctx
+): Promise<ResolversTypes['InvoiceItem'][]> => {
+  const logger = ctx.container.use('logger')
+  logger.info(args, 'getInvoiceItems args')
+  // TODO: return all invoice items for parent invoice
+  return []
+}
