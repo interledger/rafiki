c66723bb491baed3a3663adde66f71a914d40134
Cairin Michie <cairinmichie@gmail.com>
2021-07-09 17:20:12 +0200

    fix: replace JEST_WORKERS with os cpu count

--- packages/backend/jest.setup.js
+++ packages/backend/jest.setup.js
@@ -1,13 +1,15 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const Knex = require('knex')
 // eslint-disable-next-line @typescript-eslint/no-var-requires
+const { cpus } = require('os')
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const { GenericContainer } = require('testcontainers')
 
 const POSTGRES_PORT = 5432
 const REDIS_PORT = 6379
 
 module.exports = async () => {
-  const workers = parseInt(process.env.JEST_WORKERS || '1')
+  const workers = cpus().length - 1
 
   if (!process.env.DATABASE_URL) {
     const postgresContainer = await new GenericContainer('postgres')
