2a70ff2882e6247da8dfab2d4fe034f26164679d
Sabine <sabine.bertram@mailbox.org>
2021-10-22 16:41:30 +0200

    test(backend): session key management

--- packages/backend/src/sessionKey/service.test.ts
+++ packages/backend/src/sessionKey/service.test.ts
@@ -51,4 +51,29 @@ describe('Session Key Service', (): void => {
       expect(session).rejects.toThrow(new UnknownSessionError('123'))
     })
   })
+
+  describe('Manage Session', (): void => {
+    test('Can revoke a session', async (): Promise<void> => {
+      const session = await SessionKeyService.create()
+      await SessionKeyService.revoke(session.sessionKey)
+      const revokedSession = SessionKeyService.getSession(session.sessionKey)
+      expect(revokedSession).rejects.toThrow(
+        new UnknownSessionError(session.sessionKey)
+      )
+    })
+
+    test('Can renew a session', async (): Promise<void> => {
+      const session = await SessionKeyService.create()
+      const renewedSession = await SessionKeyService.renew(session.sessionKey)
+      expect(session.sessionKey).not.toEqual(renewedSession.sessionKey)
+      expect(session.expiresAt.getTime()).toBeLessThanOrEqual(
+        renewedSession.expiresAt.getTime()
+      )
+    })
+
+    test('Cannot renew non-existing session', async (): Promise<void> => {
+      const renewedSession = SessionKeyService.renew('123')
+      expect(renewedSession).rejects.toThrow(new UnknownSessionError('123'))
+    })
+  })
 })
