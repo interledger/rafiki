505010211af358dc7bd5b442f69d1547a889f911
Sabine <sabine.bertram@mailbox.org>
2021-12-03 20:03:31 +0100

    refactor(backend): fix casing

--- packages/backend/src/session/service.test.ts
+++ packages/backend/src/session/service.test.ts
@@ -9,14 +9,14 @@ import { Config } from '../config/app'
 describe('Session Key Service', (): void => {
   let deps: IocContract<AppServices>
   let appContainer: TestContainer
-  let SessionService: SessionService
+  let sessionService: SessionService
   let redis: Redis
 
   beforeAll(
     async (): Promise<void> => {
       deps = await initIocContainer(Config)
       appContainer = await createTestApp(deps)
-      SessionService = await deps.use('sessionService')
+      sessionService = await deps.use('sessionService')
       redis = await deps.use('redis')
     }
   )
@@ -36,10 +36,10 @@ describe('Session Key Service', (): void => {
 
   describe('Create / Get Session', (): void => {
     test('Can create and fetch session', async (): Promise<void> => {
-      const session = await SessionService.create()
+      const session = await sessionService.create()
       expect(session).toHaveProperty('key')
       expect(session).toHaveProperty('expiresAt')
-      const retrievedSession = await SessionService.get({
+      const retrievedSession = await sessionService.get({
         key: session.key
       })
       if (retrievedSession) {
@@ -56,24 +56,24 @@ describe('Session Key Service', (): void => {
     })
 
     test('Cannot fetch non-existing session', async (): Promise<void> => {
-      const sessionOrError = SessionService.get({ key: '123' })
+      const sessionOrError = sessionService.get({ key: '123' })
       expect(sessionOrError).resolves.toBeUndefined()
     })
   })
 
   describe('Manage Session', (): void => {
     test('Can revoke a session', async (): Promise<void> => {
-      const session = await SessionService.create()
-      await SessionService.revoke({ key: session.key })
-      const revokedSessionOrError = SessionService.get({
+      const session = await sessionService.create()
+      await sessionService.revoke({ key: session.key })
+      const revokedSessionOrError = sessionService.get({
         key: session.key
       })
       expect(revokedSessionOrError).resolves.toBeUndefined()
     })
 
     test('Can refresh a session', async (): Promise<void> => {
-      const session = await SessionService.create()
-      const refreshedSession = await SessionService.refresh({
+      const session = await sessionService.create()
+      const refreshedSession = await sessionService.refresh({
         key: session.key
       })
       if (!refreshedSession) {
@@ -90,7 +90,7 @@ describe('Session Key Service', (): void => {
     })
 
     test('Cannot refresh non-existing session', async (): Promise<void> => {
-      const refreshSessionOrError = SessionService.refresh({ key: '123' })
+      const refreshSessionOrError = sessionService.refresh({ key: '123' })
       expect(refreshSessionOrError).resolves.toBeUndefined()
     })
   })
