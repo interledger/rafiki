c133ba71bb9ac6cd1efeeabe7d1d7e2e9d6aed4a
Damien Clipp <damien@coil.com>
2021-12-13 20:34:26 -0800

    feat: working inside container only

--- packages/backend/scripts/init.sh
+++ packages/backend/scripts/init.sh
@@ -6,3 +6,9 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     CREATE DATABASE testing;
     CREATE DATABASE development;
 EOSQL
+
+# node -r ./.pnp.cjs ./dist/index.js
+
+# yarn install --immutable --immutable-cache
+# yarn workspace $INIT_CWD add backend
+echo "abcd"
