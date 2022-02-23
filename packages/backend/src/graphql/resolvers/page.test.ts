b33601a242fb6a9b6442de8e8b837fd24c132d10
Brandon Wilson <brandon@coil.com>
2022-02-23 14:24:37 -0600

    chore(backend): add common page query resolver tests

---
+++ packages/backend/src/graphql/resolvers/page.test.ts
@@ -0,0 +1,245 @@
+import { ApolloClient, NormalizedCacheObject } from '@apollo/client'
+import { gql } from 'apollo-server-koa'
+
+import { Model, PageInfo } from '../generated/graphql'
+import { BaseModel } from '../../shared/baseModel'
+
+interface PageTestsOptions<Type> {
+  getClient: () => ApolloClient<NormalizedCacheObject>
+  createModel: () => Promise<Type>
+  pagedQuery: string
+}
+
+interface Connection<Type> {
+  pageInfo: PageInfo
+  edges: Edge<Type>[]
+}
+
+interface Edge<Type> {
+  node: Type
+  cursor: string
+}
+
+export const getPageTests = <T extends Model, M extends BaseModel>({
+  getClient,
+  createModel,
+  pagedQuery
+}: PageTestsOptions<M>): void => {
+  describe('Common query resolver pagination', (): void => {
+    let apolloClient: ApolloClient<NormalizedCacheObject>
+
+    beforeAll((): void => {
+      apolloClient = getClient()
+    })
+
+    async function createModels(): Promise<M[]> {
+      const models: M[] = []
+      for (let i = 0; i < 50; i++) {
+        models.push(await createModel())
+      }
+      return models
+    }
+
+    test('pageInfo is correct on default query without params', async (): Promise<void> => {
+      const models = await createModels()
+      const query = await apolloClient
+        .query({
+          query: gql`
+            query Page {
+              ${pagedQuery} {
+                edges {
+                  node {
+                    id
+                  }
+                  cursor
+                }
+                pageInfo {
+                  endCursor
+                  hasNextPage
+                  hasPreviousPage
+                  startCursor
+                }
+              }
+            }
+          `
+        })
+        .then(
+          (query): Connection<T> => {
+            if (query.data) {
+              return query.data[pagedQuery]
+            } else {
+              throw new Error('Data was empty')
+            }
+          }
+        )
+      expect(query.edges).toHaveLength(20)
+      expect(query.pageInfo.hasNextPage).toBeTruthy()
+      expect(query.pageInfo.hasPreviousPage).toBeFalsy()
+      expect(query.pageInfo.startCursor).toEqual(models[0].id)
+      expect(query.pageInfo.endCursor).toEqual(models[19].id)
+    }, 10_000)
+
+    test('No models, but models requested', async (): Promise<void> => {
+      const query = await apolloClient
+        .query({
+          query: gql`
+            query Page {
+              ${pagedQuery} {
+                edges {
+                  node {
+                    id
+                  }
+                  cursor
+                }
+                pageInfo {
+                  endCursor
+                  hasNextPage
+                  hasPreviousPage
+                  startCursor
+                }
+              }
+            }
+          `
+        })
+        .then(
+          (query): Connection<T> => {
+            if (query.data) {
+              return query.data[pagedQuery]
+            } else {
+              throw new Error('Data was empty')
+            }
+          }
+        )
+      expect(query.edges).toHaveLength(0)
+      expect(query.pageInfo.hasNextPage).toBeFalsy()
+      expect(query.pageInfo.hasPreviousPage).toBeFalsy()
+      expect(query.pageInfo.startCursor).toBeNull()
+      expect(query.pageInfo.endCursor).toBeNull()
+    })
+
+    test('pageInfo is correct on pagination from start', async (): Promise<void> => {
+      const models = await createModels()
+      const query = await apolloClient
+        .query({
+          query: gql`
+            query Page {
+              ${pagedQuery}(first: 10) {
+                edges {
+                  node {
+                    id
+                  }
+                  cursor
+                }
+                pageInfo {
+                  endCursor
+                  hasNextPage
+                  hasPreviousPage
+                  startCursor
+                }
+              }
+            }
+          `
+        })
+        .then(
+          (query): Connection<T> => {
+            if (query.data) {
+              return query.data[pagedQuery]
+            } else {
+              throw new Error('Data was empty')
+            }
+          }
+        )
+      expect(query.edges).toHaveLength(10)
+      expect(query.pageInfo.hasNextPage).toBeTruthy()
+      expect(query.pageInfo.hasPreviousPage).toBeFalsy()
+      expect(query.pageInfo.startCursor).toEqual(models[0].id)
+      expect(query.pageInfo.endCursor).toEqual(models[9].id)
+    }, 10_000)
+
+    test('pageInfo is correct on pagination from middle', async (): Promise<void> => {
+      const models = await createModels()
+      const query = await apolloClient
+        .query({
+          query: gql`
+            query Page($after: String!) {
+              ${pagedQuery}(after: $after) {
+                edges {
+                  node {
+                    id
+                  }
+                  cursor
+                }
+                pageInfo {
+                  endCursor
+                  hasNextPage
+                  hasPreviousPage
+                  startCursor
+                }
+              }
+            }
+          `,
+          variables: {
+            after: models[19].id
+          }
+        })
+        .then(
+          (query): Connection<T> => {
+            if (query.data) {
+              return query.data[pagedQuery]
+            } else {
+              throw new Error('Data was empty')
+            }
+          }
+        )
+      expect(query.edges).toHaveLength(20)
+      expect(query.pageInfo.hasNextPage).toBeTruthy()
+      expect(query.pageInfo.hasPreviousPage).toBeTruthy()
+      expect(query.pageInfo.startCursor).toEqual(models[20].id)
+      expect(query.pageInfo.endCursor).toEqual(models[39].id)
+    }, 10_000)
+
+    test('pageInfo is correct on pagination near end', async (): Promise<void> => {
+      const models = await createModels()
+      const query = await apolloClient
+        .query({
+          query: gql`
+            query page($after: String!) {
+              ${pagedQuery}(after: $after, first: 10) {
+                edges {
+                  node {
+                    id
+                  }
+                  cursor
+                }
+                pageInfo {
+                  endCursor
+                  hasNextPage
+                  hasPreviousPage
+                  startCursor
+                }
+              }
+            }
+          `,
+          variables: {
+            after: models[44].id
+          }
+        })
+        .then(
+          (query): Connection<T> => {
+            if (query.data) {
+              return query.data[pagedQuery]
+            } else {
+              throw new Error('Data was empty')
+            }
+          }
+        )
+      expect(query.edges).toHaveLength(5)
+      expect(query.pageInfo.hasNextPage).toBeFalsy()
+      expect(query.pageInfo.hasPreviousPage).toBeTruthy()
+      expect(query.pageInfo.startCursor).toEqual(models[45].id)
+      expect(query.pageInfo.endCursor).toEqual(models[49].id)
+    }, 10_000)
+  })
+}
+
+test.todo('test suite must contain at least one test')
