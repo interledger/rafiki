1cea99f3ded00886d3e153b3c914436bc48b7ce6
Brandon Wilson <brandon@coil.com>
2022-04-07 11:57:03 -0500

    chore(backend): update tigerbeetle-node to 0.6.0 (#307)

--- packages/backend/src/tests/tigerbeetle.ts
+++ packages/backend/src/tests/tigerbeetle.ts
@@ -12,7 +12,7 @@ export async function startTigerbeetleContainer(
   const { name: tigerbeetleDir } = tmp.dirSync({ unsafeCleanup: true })
 
   await new GenericContainer(
-    'ghcr.io/coilhq/tigerbeetle@sha256:0d8cd6b7a0a7f7ef678c6fc877f294071ead642698db2a438a6599a3ade8fb6f'
+    'ghcr.io/coilhq/tigerbeetle@sha256:576957b8d4fadd03de01ab3983529d73d6767cd834aa8786340fc67fcd39cb69'
   )
     .withExposedPorts(TIGERBEETLE_PORT)
     .withBindMount(tigerbeetleDir, TIGERBEETLE_DIR)
@@ -26,7 +26,7 @@ export async function startTigerbeetleContainer(
     .start()
 
   return await new GenericContainer(
-    'ghcr.io/coilhq/tigerbeetle@sha256:0d8cd6b7a0a7f7ef678c6fc877f294071ead642698db2a438a6599a3ade8fb6f'
+    'ghcr.io/coilhq/tigerbeetle@sha256:576957b8d4fadd03de01ab3983529d73d6767cd834aa8786340fc67fcd39cb69'
   )
     .withExposedPorts(TIGERBEETLE_PORT)
     .withBindMount(tigerbeetleDir, TIGERBEETLE_DIR)
