08a26b09620a13322327bba936281bd0bc80c35b
Nathan Lie <lie4nathan@gmail.com>
2022-04-28 13:49:10 -0700

    feat: tests

--- packages/auth/jest.teardown.js
+++ packages/auth/jest.teardown.js
@@ -1,4 +1,5 @@
 module.exports = async () => {
+<<<<<<< HEAD
   await global.__AUTH_KNEX__.migrate.rollback(
     { directory: './packages/auth/migrations' },
     true
@@ -6,5 +7,14 @@ module.exports = async () => {
   await global.__AUTH_KNEX__.destroy()
   if (global.__AUTH_POSTGRES__) {
     await global.__AUTH_POSTGRES__.stop()
+=======
+  await global.__BACKEND_KNEX__.migrate.rollback(
+    { directory: './packages/backend/migrations' },
+    true
+  )
+  await global.__BACKEND_KNEX__.destroy()
+  if (global.__BACKEND_POSTGRES__) {
+    await global.__BACKEND_POSTGRES__.stop()
+>>>>>>> cd21893 (feat: tests)
   }
 }
