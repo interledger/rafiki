25f9b10c451981b42e7e800002663c9b3c683c35
Donovan Changfoot <don@fynbos.dev>
2022-05-03 16:12:36 +0200

    fix(backend): set content-type when sending ilp-packets to peer
    The content-type is expected to be set to `application/octet-stream`
    when receiving ilp-packets. We now set the content-type on outgoing
    ilp-packets.

--- packages/backend/src/connector/core/services/client.ts
+++ packages/backend/src/connector/core/services/client.ts
@@ -1,5 +1,6 @@
 import { AxiosInstance } from 'axios'
 import { Errors } from 'ilp-packet'
+import { CONTENT_TYPE } from '../middleware/ilp-packet'
 import { OutgoingAccount } from '../rafiki'
 
 export async function sendToPeer(
@@ -13,7 +14,10 @@ export async function sendToPeer(
   }
   const res = await client.post<Buffer>(http.outgoing.endpoint, prepare, {
     responseType: 'arraybuffer',
-    headers: { Authorization: `Bearer ${http.outgoing.authToken}` }
+    headers: {
+      Authorization: `Bearer ${http.outgoing.authToken}`,
+      'Content-Type': CONTENT_TYPE
+    }
   })
   return res.data
 }
