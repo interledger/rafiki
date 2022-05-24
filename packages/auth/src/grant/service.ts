ceb80c56a4cf04e23eef230d615697dc2aab58cb
Nathan Lie <lie4nathan@gmail.com>
2022-05-24 09:58:26 -0700

    fix: review comments

--- packages/auth/src/grant/service.ts
+++ packages/auth/src/grant/service.ts
@@ -78,10 +78,7 @@ function validateGrantRequest(
     if (!isAccessRequest(access)) return false
   }
 
-  return (
-    grantRequest.interact?.start !== undefined &&
-    grantRequest.interact?.finish !== undefined
-  )
+  return grantRequest.interact?.start !== undefined
 }
 
 async function initiateGrant(
