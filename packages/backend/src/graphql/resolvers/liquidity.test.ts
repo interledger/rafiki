260d9cc8edeb1f1720379744e30722fd29f1634d
Brandon Wilson <brandon@coil.com>
2022-06-03 04:22:28 -0500

    chore(backend): make receiver a url

--- packages/backend/src/graphql/resolvers/liquidity.test.ts
+++ packages/backend/src/graphql/resolvers/liquidity.test.ts
@@ -1605,7 +1605,9 @@ describe('Liquidity Resolvers', (): void => {
         })
         payment = await createOutgoingPayment(deps, {
           accountId,
-          receiver: incomingPayment.id,
+          receiver: `${
+            Config.publicHost
+          }/${uuid()}/incoming-payments/${uuid()}`,
           sendAmount: {
             value: BigInt(456),
             assetCode: account.asset.code,
