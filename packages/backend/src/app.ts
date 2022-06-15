4cccf397a16b78f7492ffdcb1bd680364de48c50
Nathan Lie <lie4nathan@gmail.com>
2022-06-15 11:32:13 -0700

    fix: backend types

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -53,7 +53,7 @@ export interface AppContextData {
   closeEmitter: EventEmitter
   container: AppContainer
   // Set by @koa/router.
-  params: { [key: string]: string }
+  params: { id: string; [key: string]: string }
   grant?: Grant
 }
 
