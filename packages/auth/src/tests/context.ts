70d1c1dad449a5b9f7c1937aa8a1aad511d07c7d
Nathan Lie <lie4nathan@gmail.com>
2022-06-16 12:09:01 -0700

    fix: actually use koa-session in tests

--- packages/auth/src/tests/context.ts
+++ packages/auth/src/tests/context.ts
@@ -1,6 +1,7 @@
 import EventEmitter from 'events'
 import * as httpMocks from 'node-mocks-http'
 import Koa from 'koa'
+import session from 'koa-session'
 
 import { AppContext, AppContextData } from '../app'
 
@@ -12,9 +13,20 @@ export function createContext(
   const res = httpMocks.createResponse()
   const koa = new Koa<unknown, AppContextData>()
   koa.keys = ['test-key']
+  koa.use(
+    session(
+      {
+        key: 'sessionId',
+        maxAge: 60 * 1000,
+        signed: true
+      },
+      // Only accepts Middleware<DefaultState, DefaultContext> for some reason, this.koa is Middleware<DefaultState, AppContext>
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      koa as any
+    )
+  )
   const ctx = koa.createContext(req, res)
   ctx.params = params
-  ctx.session = {}
   ctx.closeEmitter = new EventEmitter()
   return ctx as AppContext
 }
