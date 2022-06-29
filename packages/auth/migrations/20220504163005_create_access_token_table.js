02e88dc4d23b4eeb59b05c6d272d1a8e693bb5e8
Damien Clipp <damien@coil.com>
2022-06-29 15:41:07 -0700

    feat(auth): move migration to table creation

--- packages/auth/migrations/20220504163005_create_access_token_table.js
+++ packages/auth/migrations/20220504163005_create_access_token_table.js
@@ -9,6 +9,8 @@ exports.up = function (knex) {
 
     table.timestamp('createdAt').defaultTo(knex.fn.now())
     table.timestamp('updatedAt').defaultTo(knex.fn.now())
+
+    table.boolean('revoked').nullable()
   })
 }
 
