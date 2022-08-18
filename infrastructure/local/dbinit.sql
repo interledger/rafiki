9ca6294f56553a49ad40097a9af6963caa6272fb
Donovan Changfoot <don@fynbos.dev>
2022-08-18 16:59:55 +0200

    feat: docker compose to deploy a peer rafiki stack locally

--- infrastructure/local/dbinit.sql
+++ infrastructure/local/dbinit.sql
@@ -5,3 +5,11 @@ GRANT ALL ON DATABASE backend TO backend;
 CREATE USER auth WITH PASSWORD 'auth';
 CREATE DATABASE auth;
 GRANT ALL ON DATABASE auth TO auth;
+
+CREATE USER peerbackend WITH PASSWORD 'peerbackend';
+CREATE DATABASE peerbackend;
+GRANT ALL ON DATABASE peerbackend TO peerbackend;
+
+CREATE USER peerauth WITH PASSWORD 'peerauth';
+CREATE DATABASE peerauth;
+GRANT ALL ON DATABASE peerauth TO peerauth;
