39ab1f5ffee5f8e6b97152f63fa294355d432bb5
Matt de Haast <matt@fynbos.dev>
2022-08-19 15:57:34 +0200

    fix(testing): remove ts-node

--- jest.config.base.js
+++ jest.config.base.js
@@ -1,15 +1,9 @@
 'use strict'
 
 module.exports = {
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.build.json'
-    }
-  },
   transform: {
     '^.+\\.tsx?$': ['@swc/jest']
   },
-  preset: 'ts-jest',
   testEnvironment: 'node',
   moduleDirectories: ['node_modules', './'],
   modulePaths: ['node_modules', './']
