cfa83d383c8b8efae1c03575613cfb93c4694b2f
Travis Crist <4206970+traviscrist@users.noreply.github.com>
2022-08-23 10:31:27 +0200

    chore(deps): upgrade missing redis import

--- packages/backend/src/connector/core/factories/rafiki-services.ts
+++ packages/backend/src/connector/core/factories/rafiki-services.ts
@@ -1,6 +1,6 @@
 import * as crypto from 'crypto'
 import { Factory } from 'rosie'
-import IORedis from 'ioredis'
+import Redis from 'ioredis'
 import { StreamServer } from '@interledger/stream-receiver'
 import { RafikiServices } from '../rafiki'
 import { MockAccountingService } from '../test/mocks/accounting-service'
@@ -49,7 +49,7 @@ export const RafikiServicesFactory = Factory.define<MockRafikiServices>(
   .attr(
     'redis',
     () =>
-      new IORedis(`${process.env.REDIS_URL}/${process.env.JEST_WORKER_ID}`, {
+      new Redis(`${process.env.REDIS_URL}/${process.env.JEST_WORKER_ID}`, {
         // lazyConnect so that tests that don't use Redis don't have to disconnect it when they're finished.
         lazyConnect: true,
         stringNumbers: true
