e6ad84de2acceb6c3fccbf2b99a3c5718b0f1d5f
Donovan Changfoot <donovanchangfoot@gmail.com>
2022-08-25 14:04:15 +0200

    fix(map): json parsing

--- packages/mock-account-provider/app/routes/webhooks.ts
+++ packages/mock-account-provider/app/routes/webhooks.ts
@@ -1,7 +1,8 @@
 import type { ActionArgs } from '@remix-run/node'
 import { json } from '@remix-run/node'
 
-export function action({ request }: ActionArgs) {
-  console.log('received webhook: ', JSON.stringify(request.body))
-  return json(request.body, { status: 201 })
+export async function action({ request }: ActionArgs) {
+  const payload = await request.json()
+  console.log('received webhook: ', JSON.stringify(payload))
+  return json(undefined, { status: 200 })
 }
