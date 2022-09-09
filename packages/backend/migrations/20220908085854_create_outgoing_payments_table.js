fdc73a09aa7ab1a00cbd2149690575947be4a4c7
Sabine Schaller <sabine@coil.com>
2022-09-09 17:47:33 +0200

    style(backend): remove unnecessary styling

--- packages/backend/migrations/20220908085854_create_outgoing_payments_table.js
+++ packages/backend/migrations/20220908085854_create_outgoing_payments_table.js
@@ -1,33 +1,32 @@
 exports.up = function (knex) {
-    return knex.schema.createTable('outgoingPayments', function (table) {
-      table.uuid('id').notNullable().primary()
-      table.foreign('id').references('quotes.id')
-  
-      table.string('state').notNullable().index() // OutgoingPaymentState
-      table.string('error').nullable()
-      table.integer('stateAttempts').notNullable().defaultTo(0)
-      table.string('description').nullable()
-      table.string('externalRef').nullable()
-  
-      table.string('grantId').notNullable()
-      table.foreign('grantId').references('grants.id')
-  
-      // Open payments payment pointer corresponding to wallet account
-      // from which to request funds for payment
-      table.uuid('paymentPointerId').notNullable()
-      table.foreign('paymentPointerId').references('paymentPointers.id')
-  
-      table.uuid('peerId').nullable()
-      table.foreign('peerId').references('peers.id')
-  
-      table.timestamp('createdAt').defaultTo(knex.fn.now())
-      table.timestamp('updatedAt').defaultTo(knex.fn.now())
-  
-      table.index(['paymentPointerId', 'createdAt', 'id'])
-    })
-  }
-  
-  exports.down = function (knex) {
-    return knex.schema.dropTableIfExists('outgoingPayments')
-  }
-  
+  return knex.schema.createTable('outgoingPayments', function (table) {
+    table.uuid('id').notNullable().primary()
+    table.foreign('id').references('quotes.id')
+
+    table.string('state').notNullable().index() // OutgoingPaymentState
+    table.string('error').nullable()
+    table.integer('stateAttempts').notNullable().defaultTo(0)
+    table.string('description').nullable()
+    table.string('externalRef').nullable()
+
+    table.string('grantId').notNullable()
+    table.foreign('grantId').references('grants.id')
+
+    // Open payments payment pointer corresponding to wallet account
+    // from which to request funds for payment
+    table.uuid('paymentPointerId').notNullable()
+    table.foreign('paymentPointerId').references('paymentPointers.id')
+
+    table.uuid('peerId').nullable()
+    table.foreign('peerId').references('peers.id')
+
+    table.timestamp('createdAt').defaultTo(knex.fn.now())
+    table.timestamp('updatedAt').defaultTo(knex.fn.now())
+
+    table.index(['paymentPointerId', 'createdAt', 'id'])
+  })
+}
+
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists('outgoingPayments')
+}
