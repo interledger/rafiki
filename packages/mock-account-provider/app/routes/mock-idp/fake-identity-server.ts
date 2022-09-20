a185ebd14ca485c6201b18140f9b6b53a50fcb7c
Damien Clipp <damien@coil.com>
2022-09-19 18:45:53 -0700

    feat(auth): fix prettier

--- packages/mock-account-provider/app/routes/mock-idp/fake-identity-server.ts
+++ packages/mock-account-provider/app/routes/mock-idp/fake-identity-server.ts
@@ -2,7 +2,7 @@ import { LoaderArgs, Response } from '@remix-run/node'
 
 export function loader({ request }: LoaderArgs) {
   const includeHeaders = ['signature', 'signature-input', 'x-idp-secret']
-  let responseHeaders: Record<string, string> = {}
+  const responseHeaders: Record<string, string> = {}
 
   includeHeaders.forEach((h) => {
     if (request.headers.has(h)) {
