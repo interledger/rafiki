3ab2b3279d1450001d8e9b53a2159f5a2ad45b2a
Damien Clipp <damienclipp@MacBook-Air.local>
2022-09-29 17:20:56 -0700

    feat(backend): fix httpsig pkg

--- packages/http-message-signatures/lib/digest/index.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="node" />
-import { DigestAlgorithm, RequestLike } from '../types';
-export declare function createContentDigestHeader(body: string | Buffer | undefined, algorithms: DigestAlgorithm[]): string;
-export declare function verifyContentDigest(request: RequestLike): boolean;
--- packages/http-message-signatures/lib/index.d.ts
+++
@@ -1,6 +0,0 @@
-export * from './algorithm';
-export * from './digest';
-export * from './types';
-export * as default from './httpbis';
-export * as httpis from './httpbis';
-export * as cavage from './cavage';
