cfa602f9c67a052c898ed30aa5fb332994fe159a
Sabine Schaller <sabine@coil.com>
2022-10-12 13:06:11 +0200

    test(backend): add Brandon's suggestions

--- packages/backend/src/config/app.test.ts
+++ packages/backend/src/config/app.test.ts
@@ -16,19 +16,35 @@ describe('Config', (): void => {
       fs.rmSync(TMP_DIR, { recursive: true, force: true })
     })
 
-    test('can provision key', async (): Promise<void> => {
-      expect(fs.existsSync(TMP_DIR)).toBe(false)
-      expect(fs.existsSync(PRIVATE_KEY_FILE)).toBe(false)
-      const { key, jwk } = parseOrProvisionKey(PRIVATE_KEY_FILE)
-      expect(key).toBeInstanceOf(crypto.KeyObject)
-      expect(jwk).toMatchObject({
-        crv: 'Ed25519',
-        kty: 'OKP',
-        d: expect.any(String),
-        x: expect.any(String)
-      })
-      expect(fs.existsSync(PRIVATE_KEY_FILE)).toBe(true)
-    })
+    test.each`
+      tmpDirExists
+      ${false}
+      ${true}
+    `(
+      'can provision key - tmp dir exists: $tmpDirExists',
+      async ({ tmpDirExists }): Promise<void> => {
+        if (tmpDirExists) {
+          fs.mkdirSync(TMP_DIR)
+        }
+        expect(fs.existsSync(TMP_DIR)).toBe(tmpDirExists)
+        expect(fs.existsSync(PRIVATE_KEY_FILE)).toBe(false)
+        const { key, jwk } = parseOrProvisionKey(PRIVATE_KEY_FILE)
+        expect(key).toMatchObject({
+          asymmetricKeyType: 'ed25519',
+          type: 'private'
+        })
+        expect(jwk).toEqual(key.export({ format: 'jwk' }))
+        expect(jwk).toEqual({
+          crv: 'Ed25519',
+          kty: 'OKP',
+          d: expect.any(String),
+          x: expect.any(String)
+        })
+        expect(fs.readFileSync(PRIVATE_KEY_FILE, 'utf8')).toEqual(
+          key.export({ format: 'pem', type: 'pkcs8' })
+        )
+      }
+    )
     test('can parse key', async (): Promise<void> => {
       const keypair = crypto.generateKeyPairSync('ed25519')
       const keyfile = `${TMP_DIR}/test-private-key.pem`
@@ -41,7 +57,7 @@ describe('Config', (): void => {
       const fileStats = fs.statSync(keyfile)
       const { key, jwk } = parseOrProvisionKey(keyfile)
       expect(key).toBeInstanceOf(crypto.KeyObject)
-      expect(jwk).toMatchObject({
+      expect(jwk).toEqual({
         crv: 'Ed25519',
         kty: 'OKP',
         d: expect.any(String),
