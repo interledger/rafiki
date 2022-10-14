120dcae03cc70ce8a065a602cec55b7b527b309a
maxkurapov@gmail.com <max.kurapov@neofinancial.com>
2022-10-14 17:50:20 +0200

    feat(open-payments): pin the open api spec to the most recent commit

--- packages/open-payments/src/config.ts
+++ packages/open-payments/src/config.ts
@@ -1,5 +1,5 @@
 export default {
   OPEN_PAYMENTS_OPEN_API_URL:
-    'https://raw.githubusercontent.com/interledger/open-payments/main/open-api-spec.yaml',
+    'https://raw.githubusercontent.com/interledger/open-payments/4c873dba89164decffbe84905d12f1d4ec045389/open-api-spec.yaml',
   DEFAULT_REQUEST_TIMEOUT: 3_000
 }
