ac71e7cf8653ab79c157660c24703dfefba61c34
Max Kurapov <max@interledger.org>
2022-11-07 17:50:25 +0100

    chore(open-payments): update AS request/response types

--- packages/open-payments/src/types.ts
+++ packages/open-payments/src/types.ts
@@ -4,7 +4,8 @@ import {
 } from './generated/resource-server-types'
 import {
   components as ASComponents,
-  paths as ASPaths
+  paths as ASPaths,
+  operations as ASOperations
 } from './generated/authorization-server-types'
 
 export const getRSPath = <P extends keyof RSPaths>(path: P): string =>
@@ -18,17 +19,17 @@ export type PaymentPointer = RSComponents['schemas']['payment-pointer']
 export const getASPath = <P extends keyof ASPaths>(path: P): string =>
   path as string
 export type NonInteractiveGrantRequest = {
-  accessToken: ASComponents['schemas']['access_token']
-  client: ASComponents['schemas']['client']
+  access_token: ASOperations['post-request']['requestBody']['content']['application/json']['access_token']
+  client: ASOperations['post-request']['requestBody']['content']['application/json']['client']
 }
 export type NonInteractiveGrant = {
-  accessToken: ASComponents['schemas']['access_token']
+  access_token: ASComponents['schemas']['access_token']
   continue: ASComponents['schemas']['continue']
 }
 export type InteractiveGrantRequest = {
-  accessToken: ASComponents['schemas']['access_token']
-  client: ASComponents['schemas']['client']
-  interact: ASComponents['schemas']['interact-request']
+  access_token: ASOperations['post-request']['requestBody']['content']['application/json']['access_token']
+  client: ASOperations['post-request']['requestBody']['content']['application/json']['client']
+  interact: ASOperations['post-request']['requestBody']['content']['application/json']['interact']
 }
 export type InteractiveGrant = {
   interact: ASComponents['schemas']['interact-response']
