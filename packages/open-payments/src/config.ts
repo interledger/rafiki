0a453d5030ba1873a7aa67a8d6c5a933f87667c9
Max Kurapov <max@interledger.org>
2022-11-17 00:35:42 +0100

    chore(open-payments): update open api specs

--- packages/open-payments/src/config.ts
+++ packages/open-payments/src/config.ts
@@ -1,7 +1,7 @@
 export default {
   OPEN_PAYMENTS_RS_OPEN_API_URL:
-    'https://raw.githubusercontent.com/interledger/open-payments/62c4b4a9875e3adaa21f89f597e88db43016fe0b/openapi/resource-server.yaml',
+    'https://raw.githubusercontent.com/interledger/open-payments/0523804eeb21d9b8cd2eb65e4119738a0512a095/openapi/resource-server.yaml',
   OPEN_PAYMENTS_AS_OPEN_API_URL:
-    'https://raw.githubusercontent.com/interledger/open-payments/62c4b4a9875e3adaa21f89f597e88db43016fe0b/openapi/auth-server.yaml',
+    'https://raw.githubusercontent.com/interledger/open-payments/0523804eeb21d9b8cd2eb65e4119738a0512a095/openapi/auth-server.yaml',
   DEFAULT_REQUEST_TIMEOUT_MS: 5_000
 }
