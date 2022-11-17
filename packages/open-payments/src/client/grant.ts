6eff96c528e937c7ae2d0c551b80179dc10b01d0
Max Kurapov <max@interledger.org>
2022-11-17 01:25:57 +0100

    feat(open-payments): update grant request

--- packages/open-payments/src/client/grant.ts
+++ packages/open-payments/src/client/grant.ts
@@ -1,4 +1,4 @@
-import { HttpMethod } from 'openapi'
+import { HttpMethod, ResponseValidator } from 'openapi'
 import { RouteDeps } from '.'
 import {
   getASPath,
@@ -26,33 +26,92 @@ export interface GrantRoutes {
 export const createGrantRoutes = (deps: RouteDeps): GrantRoutes => {
   const { axiosInstance, openApi, logger } = deps
 
-  const createInteractiveGrantValidator =
-    openApi.createResponseValidator<InteractiveGrant>({
-      path: getASPath('/'),
-      method: HttpMethod.POST
-    })
-  const createNonInteractiveGrantValidator =
-    openApi.createResponseValidator<NonInteractiveGrant>({
-      path: getASPath('/'),
-      method: HttpMethod.POST
-    })
-
+  const requestGrantValidator = openApi.createResponseValidator<
+    InteractiveGrant | NonInteractiveGrant
+  >({
+    path: getASPath('/'),
+    method: HttpMethod.POST
+  })
   return {
     requestInteractiveGrant: (
       args: RequestGrantArgs<InteractiveGrantRequest>
     ) =>
-      post(
+      requestInteractiveGrant(
         { axiosInstance, logger },
-        { url: args.url, body: args.request },
-        createInteractiveGrantValidator
+        args,
+        requestGrantValidator
       ),
     requestNonInteractiveGrant: (
       args: RequestGrantArgs<NonInteractiveGrantRequest>
     ) =>
-      post(
+      requestNonInteractiveGrant(
         { axiosInstance, logger },
-        { url: args.url, body: args.request },
-        createNonInteractiveGrantValidator
+        args,
+        requestGrantValidator
       )
   }
 }
+
+export const requestInteractiveGrant = async (
+  deps: Pick<RouteDeps, 'axiosInstance' | 'logger'>,
+  args: RequestGrantArgs<InteractiveGrantRequest>,
+  validateOpenApiResponse: ResponseValidator<
+    InteractiveGrant | NonInteractiveGrant
+  >
+) => {
+  const { axiosInstance, logger } = deps
+  const { url } = args
+
+  const grant = await post(
+    { axiosInstance, logger },
+    { url: args.url, body: args.request },
+    validateOpenApiResponse
+  )
+
+  if (!isInteractiveGrant(grant)) {
+    const errorMessage = 'Could not validate interactive grant'
+    logger.error({ url, grant }, errorMessage)
+    throw new Error(errorMessage)
+  }
+
+  return grant
+}
+
+export const requestNonInteractiveGrant = async (
+  deps: Pick<RouteDeps, 'axiosInstance' | 'logger'>,
+  args: RequestGrantArgs<NonInteractiveGrantRequest>,
+  validateOpenApiResponse: ResponseValidator<
+    InteractiveGrant | NonInteractiveGrant
+  >
+) => {
+  const { axiosInstance, logger } = deps
+  const { url } = args
+
+  const grant = await post(
+    { axiosInstance, logger },
+    { url: args.url, body: args.request },
+    validateOpenApiResponse
+  )
+
+  if (!isNonInteractiveGrant(grant)) {
+    const errorMessage = 'Could not validate non-interactive grant'
+    logger.error({ url, grant }, errorMessage)
+    throw new Error(errorMessage)
+  }
+
+  return grant
+}
+
+export const isInteractiveGrant = (
+  grant: InteractiveGrant | NonInteractiveGrant
+): grant is InteractiveGrant =>
+  !('access_token' in grant) &&
+  'interact' in grant &&
+  !!(grant as InteractiveGrant).interact
+
+export const isNonInteractiveGrant = (
+  grant: InteractiveGrant | NonInteractiveGrant
+): grant is NonInteractiveGrant =>
+  !('interact' in grant) &&
+  'access_token' in grant &&
+  !!(grant as NonInteractiveGrant).access_token
