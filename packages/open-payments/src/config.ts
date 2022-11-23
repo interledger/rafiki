f70b627b6c440bf12cb737f4ef6ed5078f8e38b6
Brandon Wilson <brandon@coil.com>
2022-11-23 07:34:42 -0600

    chore(auth): update OpenAPI specs

--- packages/open-payments/src/config.ts
+++ packages/open-payments/src/config.ts
@@ -1,7 +1,7 @@
 export default {
   OPEN_PAYMENTS_RS_OPEN_API_URL:
-    'https://raw.githubusercontent.com/interledger/open-payments/56d441fe7f1d44f9111ae1a37de536e72407038d/openapi/resource-server.yaml',
+    'https://raw.githubusercontent.com/interledger/open-payments/b363d33038fe789e5388f04f80ddd06a4fa97093/openapi/resource-server.yaml',
   OPEN_PAYMENTS_AS_OPEN_API_URL:
-    'https://raw.githubusercontent.com/interledger/open-payments/56d441fe7f1d44f9111ae1a37de536e72407038d/openapi/auth-server.yaml',
+    'https://raw.githubusercontent.com/interledger/open-payments/b363d33038fe789e5388f04f80ddd06a4fa97093/openapi/auth-server.yaml',
   DEFAULT_REQUEST_TIMEOUT_MS: 5_000
 }
