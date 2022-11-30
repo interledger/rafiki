f6eae9c2cd37cf4ba0c8ccdb9d97f3e361b248e2
Sabine Schaller <sabine@coil.com>
2022-11-30 13:58:47 +0100

    feat(HSU): move content digest creation into HSU

--- packages/http-signature-utils/src/index.ts
+++ packages/http-signature-utils/src/index.ts
@@ -1,3 +1,4 @@
 export { generateJwk } from './utils/jwk'
 export { parseOrProvisionKey } from './utils/key'
 export { createSignatureHeaders } from './utils/signatures'
+export { createContentHeaders } from './utils/content_headers'
