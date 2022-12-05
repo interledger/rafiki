465b537fe19a417ce45a79fe90cdefb88f661b6c
Max Kurapov <max@interledger.org>
2022-12-05 12:02:28 +0100

    663/mk/create outgoing payment (#807)
    * feat(open-payments): create outgoing payment
    
    * chore(backend): fix addSentAmount fn
    
    * chore(open-payments): bye console.log
    
    * feat(open-payments): add test for POST with accessToken
    
    * chore(open-payments): update quoteId in test
    
    * chore(open-payments): prettier
    
    * chore(open-payments): address feedback, update tests
    
    * chore(open-payments): update tests

--- packages/open-payments/src/client/outgoing-payment.test.ts
+++ packages/open-payments/src/client/outgoing-payment.test.ts
@@ -1,4 +1,5 @@
 import {
+  createOutgoingPayment,
   createOutgoingPaymentRoutes,
   getOutgoingPayment,
   validateOutgoingPayment
@@ -12,6 +13,7 @@ import {
   silentLogger
 } from '../test/helpers'
 import nock from 'nock'
+import { v4 as uuid } from 'uuid'
 
 describe('outgoing-payment', (): void => {
   let openApi: OpenAPI
@@ -39,13 +41,27 @@ describe('outgoing-payment', (): void => {
         method: HttpMethod.GET
       })
     })
+
+    test('creates createOutgoingPaymentOpenApiValidator properly', async (): Promise<void> => {
+      jest.spyOn(openApi, 'createResponseValidator')
+
+      createOutgoingPaymentRoutes({
+        axiosInstance,
+        openApi,
+        logger
+      })
+      expect(openApi.createResponseValidator).toHaveBeenCalledWith({
+        path: '/outgoing-payments',
+        method: HttpMethod.POST
+      })
+    })
   })
 
   describe('getOutgoingPayment', (): void => {
     test('returns outgoing payment if passes validation', async (): Promise<void> => {
       const outgoingPayment = mockOutgoingPayment()
 
-      nock(baseUrl).get('/outgoing-payment').reply(200, outgoingPayment)
+      nock(baseUrl).get('/outgoing-payments').reply(200, outgoingPayment)
 
       const result = await getOutgoingPayment(
         {
@@ -53,7 +69,7 @@ describe('outgoing-payment', (): void => {
           logger
         },
         {
-          url: `${baseUrl}/outgoing-payment`,
+          url: `${baseUrl}/outgoing-payments`,
           accessToken: 'accessToken'
         },
         openApiValidators.successfulValidator
@@ -84,7 +100,7 @@ describe('outgoing-payment', (): void => {
             logger
           },
           {
-            url: `${baseUrl}/outgoing-payment`,
+            url: `${baseUrl}/outgoing-payments`,
             accessToken: 'accessToken'
           },
           openApiValidators.successfulValidator
@@ -92,7 +108,7 @@ describe('outgoing-payment', (): void => {
       ).rejects.toThrowError()
     })
 
-    test('throws is outgoing payment does not pass open api validation', async (): Promise<void> => {
+    test('throws if outgoing payment does not pass open api validation', async (): Promise<void> => {
       const outgoingPayment = mockOutgoingPayment()
 
       nock(baseUrl).get('/outgoing-payment').reply(200, outgoingPayment)
@@ -104,12 +120,117 @@ describe('outgoing-payment', (): void => {
             logger
           },
           {
-            url: `${baseUrl}/outgoing-payment`,
+            url: `${baseUrl}/outgoing-payments`,
             accessToken: 'accessToken'
           },
           openApiValidators.failedValidator
         )
       ).rejects.toThrowError()
+    })
+  })
+
+  describe('createOutgoingPayment', (): void => {
+    const quoteId = `${baseUrl}/quotes/${uuid()}`
+
+    test.each`
+      description           | externalRef
+      ${'Some description'} | ${'#INV-1'}
+      ${undefined}          | ${undefined}
+    `(
+      'creates outgoing payment',
+      async ({ description, externalRef }): Promise<void> => {
+        const outgoingPayment = mockOutgoingPayment({
+          quoteId,
+          description,
+          externalRef
+        })
+
+        const scope = nock(baseUrl)
+          .post('/outgoing-payments')
+          .reply(200, outgoingPayment)
+
+        const result = await createOutgoingPayment(
+          {
+            axiosInstance,
+            logger
+          },
+          {
+            url: `${baseUrl}/outgoing-payments`,
+            accessToken: 'accessToken',
+            body: {
+              quoteId,
+              description,
+              externalRef
+            }
+          },
+          openApiValidators.successfulValidator
+        )
+        expect(result).toEqual(outgoingPayment)
+        scope.done()
+      }
+    )
+
+    test('throws if outgoing payment does not pass validation', async (): Promise<void> => {
+      const outgoingPayment = mockOutgoingPayment({
+        sendAmount: {
+          assetCode: 'USD',
+          assetScale: 3,
+          value: '5'
+        },
+        sentAmount: {
+          assetCode: 'CAD',
+          assetScale: 3,
+          value: '0'
+        }
+      })
+
+      const scope = nock(baseUrl)
+        .post('/outgoing-payments')
+        .reply(200, outgoingPayment)
+
+      await expect(() =>
+        createOutgoingPayment(
+          {
+            axiosInstance,
+            logger
+          },
+          {
+            url: `${baseUrl}/outgoing-payments`,
+            accessToken: 'accessToken',
+            body: {
+              quoteId: uuid()
+            }
+          },
+          openApiValidators.successfulValidator
+        )
+      ).rejects.toThrowError()
+      scope.done()
+    })
+
+    test('throws if outgoing payment does not pass open api validation', async (): Promise<void> => {
+      const outgoingPayment = mockOutgoingPayment()
+
+      const scope = nock(baseUrl)
+        .post('/outgoing-payments')
+        .reply(200, outgoingPayment)
+
+      await expect(() =>
+        createOutgoingPayment(
+          {
+            axiosInstance,
+            logger
+          },
+          {
+            url: `${baseUrl}/outgoing-payments`,
+            accessToken: 'accessToken',
+            body: {
+              quoteId: uuid()
+            }
+          },
+          openApiValidators.failedValidator
+        )
+      ).rejects.toThrowError()
+      scope.done()
     })
   })
 
