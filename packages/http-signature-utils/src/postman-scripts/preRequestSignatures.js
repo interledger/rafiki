1276e17865d7561d7123210681ab4cfa186d5ecc
Sabine Schaller <sabine@coil.com>
2022-12-13 14:49:03 +0100

    fix(HSU): formatting

--- packages/http-signature-utils/src/postman-scripts/preRequestSignatures.js
+++ packages/http-signature-utils/src/postman-scripts/preRequestSignatures.js
@@ -1,29 +1,45 @@
-const requestUrl = request.url.replace(/{{([A-Za-z]\w+)}}/g, (_, key) => pm.collectionVariables.get(key)).replace(/localhost:([3,4])000/g, (_, key) => key === '3'? 'backend' : 'peer-backend')
-const requestBody = request.method === 'POST' ? request.data.replace(/{{([A-Za-z]\w+)}}/g, (_, key) => pm.collectionVariables.get(key)): undefined
-const requestHeaders = JSON.parse(JSON.stringify(request.headers).replace(/{{([A-Za-z]\w+)}}/g, (_, key) => pm.collectionVariables.get(key)))
+const requestUrl = request.url
+  .replace(/{{([A-Za-z]\w+)}}/g, (_, key) => pm.collectionVariables.get(key))
+  .replace(/localhost:([3,4])000/g, (_, key) =>
+    key === '3' ? 'backend' : 'peer-backend'
+  )
+const requestBody =
+  request.method === 'POST'
+    ? request.data.replace(/{{([A-Za-z]\w+)}}/g, (_, key) =>
+        pm.collectionVariables.get(key)
+      )
+    : undefined
+const requestHeaders = JSON.parse(
+  JSON.stringify(request.headers).replace(/{{([A-Za-z]\w+)}}/g, (_, key) =>
+    pm.collectionVariables.get(key)
+  )
+)
 
 // Request Signature Headers
-pm.sendRequest({
+pm.sendRequest(
+  {
     url: pm.collectionVariables.get('signatureUrl'),
     method: 'POST',
     header: {
-        'content-type': 'application/json'
+      'content-type': 'application/json'
     },
-    body:{
-        mode: 'raw',
-        raw: JSON.stringify({
-            keyId: pm.collectionVariables.get('keyId'),
-            request: {
-                url: requestUrl,
-                method: request.method,
-                headers: requestHeaders,
-                body: requestBody
-            }
-        })
+    body: {
+      mode: 'raw',
+      raw: JSON.stringify({
+        keyId: pm.collectionVariables.get('keyId'),
+        request: {
+          url: requestUrl,
+          method: request.method,
+          headers: requestHeaders,
+          body: requestBody
+        }
+      })
     }
-}, (_, res) => {
+  },
+  (_, res) => {
     const headers = res.json()
     for (let [key, value] of Object.entries(headers)) {
-        pm.request.headers.add({key, value});
+      pm.request.headers.add({ key, value })
     }
-})
+  }
+)
