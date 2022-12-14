ac84eb1abe267bb742c32881c650e31f0a297a25
Nathan Lie <lie4nathan@gmail.com>
2022-12-14 09:34:15 -0800

    feat(auth): add typings to request contexts & bodies (#821)

--- packages/openapi/src/middleware.ts
+++ packages/openapi/src/middleware.ts
@@ -2,7 +2,7 @@ import { OpenAPI, RequestOptions, isValidationError } from './'
 
 import Koa from 'koa'
 
-export function createValidatorMiddleware<T extends Koa.ParameterizedContext>(
+export function createValidatorMiddleware<T extends Koa.DefaultContext>(
   spec: OpenAPI,
   options: RequestOptions
 ): (ctx: Koa.Context, next: () => Promise<unknown>) => Promise<void> {
@@ -31,6 +31,7 @@ export function createValidatorMiddleware<T extends Koa.ParameterizedContext>(
       } else if (isValidationError(err)) {
         ctx.throw(err.status ?? 500, err.errors[0])
       } else {
+        console.log('err=', err)
         ctx.throw(500)
       }
     }
