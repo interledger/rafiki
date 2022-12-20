cdf3db6d59d337cb1969da438a5568111d1fb3f7
Max Kurapov <max@interledger.org>
2022-12-20 14:18:43 +0100

    chore(hsu): enable strict type checking & fix types

--- packages/http-signature-utils/src/utils/headers.ts
+++ packages/http-signature-utils/src/utils/headers.ts
@@ -29,7 +29,7 @@ export const createHeaders = async ({
   privateKey,
   keyId
 }: SignOptions): Promise<Headers> => {
-  let contentHeaders: ContentHeaders
+  let contentHeaders: ContentHeaders | undefined
   if (request.body) {
     contentHeaders = createContentHeaders(request.body as string)
     request.headers = { ...request.headers, ...contentHeaders }
