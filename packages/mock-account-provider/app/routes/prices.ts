33a418b2baa0c47779d3e99171c324a35284acca
Radu-Cristian Popa <praducristian@gmail.com>
2023-01-13 08:34:13 +0200

    feat(map): update `prices` route

--- packages/mock-account-provider/app/routes/prices.ts
+++ packages/mock-account-provider/app/routes/prices.ts
@@ -4,9 +4,11 @@ import { json } from '@remix-run/node'
 export function loader({ request }: LoaderArgs) {
   return json(
     {
-      'USD:USD': '1.0',
-      'USD:EUR': '1.1602',
-      'USD:ZAR': '17.3792'
+      base: 'USD',
+      rates: {
+        EUR: 1.1602,
+        ZAR: 17.3792
+      }
     },
     { status: 200 }
   )
