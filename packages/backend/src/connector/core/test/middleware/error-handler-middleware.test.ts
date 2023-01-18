d63ed341e2b774d4270032b76e28bb7f407b6ce0
Sabine Schaller <sabine@coil.com>
2023-01-18 17:49:35 +0100

    test(backend): test for ilpError format in reject

--- packages/backend/src/connector/core/test/middleware/error-handler-middleware.test.ts
+++ packages/backend/src/connector/core/test/middleware/error-handler-middleware.test.ts
@@ -1,6 +1,8 @@
 import { createILPContext } from '../../utils'
 import { RafikiServicesFactory } from '../../factories'
 import { createIncomingErrorHandlerMiddleware } from '../../middleware/error-handler'
+import { errorToIlpReject, IlpErrorCode, isIlpError } from 'ilp-packet'
+import assert from 'assert'
 
 describe('Error Handler Middleware', () => {
   const ADDRESS = 'test.rafiki'
@@ -23,6 +25,12 @@ describe('Error Handler Middleware', () => {
       },
       'Error thrown in incoming pipeline'
     )
+    expect(ctx.response.reject).toEqual({
+      message: 'unexpected internal error',
+      code: IlpErrorCode.T00_INTERNAL_ERROR,
+      triggeredBy: ADDRESS,
+      data: expect.any(Object)
+    })
     expect(ctx.revertTotalReceived).toBeUndefined()
   })
 
@@ -30,15 +38,15 @@ describe('Error Handler Middleware', () => {
     const ctx = createILPContext({ services })
     const errorToBeThrown = new Error('Test Error')
     errorToBeThrown['ilpErrorCode'] = 'T00'
-
+    assert.ok(isIlpError(errorToBeThrown))
     const next = jest.fn().mockImplementation(() => {
       throw errorToBeThrown
     })
     const middleware = createIncomingErrorHandlerMiddleware(ADDRESS)
-
     await expect(middleware(ctx, next)).resolves.toBeUndefined()
-
-    expect(ctx.response.reject).toBeDefined()
+    expect(ctx.response.reject).toEqual(
+      errorToIlpReject(ADDRESS, errorToBeThrown)
+    )
     expect(ctx.services.logger.debug).toHaveBeenCalledWith(
       {
         err: errorToBeThrown
@@ -59,8 +67,12 @@ describe('Error Handler Middleware', () => {
     await expect(middleware(ctx, next)).resolves.toBeUndefined()
 
     expect(ctx.response.reject).toBeDefined()
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    expect(ctx.response.reject!.triggeredBy).toEqual(ADDRESS)
+    expect(ctx.response.reject).toEqual({
+      message: 'unexpected internal error',
+      code: IlpErrorCode.T00_INTERNAL_ERROR,
+      triggeredBy: ADDRESS,
+      data: expect.any(Object)
+    })
     expect(ctx.revertTotalReceived).toBeUndefined()
   })
 
@@ -77,6 +89,12 @@ describe('Error Handler Middleware', () => {
     expect(ctx.services.logger.error).toHaveBeenCalledWith(
       'handler did not return a valid value.'
     )
+    expect(ctx.response.reject).toEqual({
+      message: 'unexpected internal error',
+      code: IlpErrorCode.T00_INTERNAL_ERROR,
+      triggeredBy: ADDRESS,
+      data: expect.any(Object)
+    })
     expect(ctx.revertTotalReceived).toBeUndefined()
   })
 
@@ -92,7 +110,12 @@ describe('Error Handler Middleware', () => {
     await expect(middleware(ctx, next)).resolves.toBeUndefined()
 
     expect(ctx.response.reject).toBeDefined()
-
+    expect(ctx.response.reject).toEqual({
+      message: 'unexpected internal error',
+      code: IlpErrorCode.T00_INTERNAL_ERROR,
+      triggeredBy: ADDRESS,
+      data: expect.any(Object)
+    })
     expect(ctx.revertTotalReceived).toEqual(expect.any(Function))
     expect(ctx.revertTotalReceived).toHaveBeenCalledTimes(1)
   })
