454fe3ed0b21c8034c46566c22e97e995088918a
Max Kurapov <max@interledger.org>
2023-02-06 12:43:21 +0100

    feat(backend): create stub psqlAccountingService

---
+++ packages/backend/src/accounting/psql/service.ts
@@ -0,0 +1,138 @@
+import { BaseService } from '../../shared/baseService'
+import { TransferError } from '../errors'
+import {
+  AccountingService,
+  LiquidityAccountTypes,
+  Deposit,
+  LiquidityAccount,
+  Transaction,
+  TransferOptions,
+  Withdrawal
+} from '../service'
+
+export interface ServiceDependencies extends BaseService {
+  withdrawalThrottleDelay?: number
+}
+
+export function createAccountingService(
+  deps_: ServiceDependencies
+): AccountingService {
+  const deps = {
+    ...deps_,
+    logger: deps_.logger.child({ service: 'PsqlAccountingService' })
+  }
+  return {
+    createLiquidityAccount: (options, accTypeCode) =>
+      createLiquidityAccount(deps, options, accTypeCode),
+    createSettlementAccount: (ledger) => createSettlementAccount(deps, ledger),
+    getBalance: (id) => getAccountBalance(deps, id),
+    getTotalSent: (id) => getAccountTotalSent(deps, id),
+    getAccountsTotalSent: (ids) => getAccountsTotalSent(deps, ids),
+    getTotalReceived: (id) => getAccountTotalReceived(deps, id),
+    getAccountsTotalReceived: (ids) => getAccountsTotalReceived(deps, ids),
+    getSettlementBalance: (ledger) => getSettlementBalance(deps, ledger),
+    createTransfer: (options) => createTransfer(deps, options),
+    createDeposit: (transfer) => createAccountDeposit(deps, transfer),
+    createWithdrawal: (transfer) => createAccountWithdrawal(deps, transfer),
+    postWithdrawal: (options) => postAccountWithdrawal(deps, options),
+    voidWithdrawal: (options) => voidAccountWithdrawal(deps, options)
+  }
+}
+
+export async function createLiquidityAccount(
+  deps: ServiceDependencies,
+  account: LiquidityAccount,
+  accountType?: LiquidityAccountTypes
+): Promise<LiquidityAccount> {
+  throw new Error('Not implemented')
+}
+
+export async function createSettlementAccount(
+  deps: ServiceDependencies,
+  ledger: number
+): Promise<void> {
+  throw new Error('Not implemented')
+}
+
+export async function getAccountBalance(
+  deps: ServiceDependencies,
+  id: string
+): Promise<bigint | undefined> {
+  throw new Error('Not implemented')
+}
+
+export async function getAccountTotalSent(
+  deps: ServiceDependencies,
+  id: string
+): Promise<bigint | undefined> {
+  throw new Error('Not implemented')
+}
+
+export async function getAccountsTotalSent(
+  deps: ServiceDependencies,
+  ids: string[]
+): Promise<(bigint | undefined)[]> {
+  throw new Error('Not implemented')
+}
+
+export async function getAccountTotalReceived(
+  deps: ServiceDependencies,
+  id: string
+): Promise<bigint | undefined> {
+  throw new Error('Not implemented')
+}
+
+export async function getAccountsTotalReceived(
+  deps: ServiceDependencies,
+  ids: string[]
+): Promise<(bigint | undefined)[]> {
+  throw new Error('Not implemented')
+}
+
+export async function getSettlementBalance(
+  deps: ServiceDependencies,
+  ledger: number
+): Promise<bigint | undefined> {
+  throw new Error('Not implemented')
+}
+
+export async function createTransfer(
+  deps: ServiceDependencies,
+  {
+    sourceAccount,
+    destinationAccount,
+    sourceAmount,
+    destinationAmount,
+    timeout
+  }: TransferOptions
+): Promise<Transaction | TransferError> {
+  throw new Error('Not implemented')
+}
+
+async function createAccountDeposit(
+  deps: ServiceDependencies,
+  { id, account, amount }: Deposit
+): Promise<void | TransferError> {
+  throw new Error('Not implemented')
+}
+
+async function createAccountWithdrawal(
+  deps: ServiceDependencies,
+  { id, account, amount, timeout }: Withdrawal
+): Promise<void | TransferError> {
+  throw new Error('Not implemented')
+}
+
+async function voidAccountWithdrawal(
+  deps: ServiceDependencies,
+  withdrawalId: string
+): Promise<void | TransferError> {
+  throw new Error('Not implemented')
+}
+
+async function postAccountWithdrawal(
+  deps: ServiceDependencies,
+  withdrawalId: string
+): Promise<void | TransferError> {
+  throw new Error('Not implemented')
+}
