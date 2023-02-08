8860991b4ec834e64f0466044a11af029bc90699
Sabine Schaller <sabine@coil.com>
2023-02-08 14:35:10 +0100

    test(backend): do not stop individual tb containers

--- packages/backend/src/accounting/tigerbeetle/service.test.ts
+++ packages/backend/src/accounting/tigerbeetle/service.test.ts
@@ -1,5 +1,4 @@
 import assert from 'assert'
-import { StartedTestContainer } from 'testcontainers'
 import { CreateAccountError as CreateTbAccountError } from 'tigerbeetle-node'
 import { v4 as uuid } from 'uuid'
 
@@ -26,7 +25,7 @@ describe('Accounting Service', (): void => {
   let appContainer: TestContainer
   let accountingService: AccountingService
   let accountFactory: AccountFactory
-  let tigerbeetleContainer: StartedTestContainer
+  // let tigerbeetleContainer: StartedTestContainer
   const timeout = BigInt(10_000) // 10 seconds
 
   let ledger = 1
@@ -35,8 +34,7 @@ describe('Accounting Service', (): void => {
   }
 
   beforeAll(async (): Promise<void> => {
-    const { container, port } = await startTigerbeetleContainer()
-    tigerbeetleContainer = container
+    const { port } = await startTigerbeetleContainer()
     Config.tigerbeetleReplicaAddresses = [port]
 
     deps = await initIocContainer(Config)
@@ -51,8 +49,6 @@ describe('Accounting Service', (): void => {
 
   afterAll(async (): Promise<void> => {
     await appContainer.shutdown()
-    await new Promise((f) => setTimeout(f, 5000))
-    await tigerbeetleContainer.stop()
   })
 
   describe('Create Liquidity Account', (): void => {
--- packages/backend/src/asset/service.test.ts
+++ packages/backend/src/asset/service.test.ts
@@ -1,5 +1,4 @@
 import assert from 'assert'
-import { StartedTestContainer } from 'testcontainers'
 import { v4 as uuid } from 'uuid'
 
 import { AssetError, isAssetError } from './errors'
@@ -21,11 +20,9 @@ describe('Asset Service', (): void => {
   let deps: IocContract<AppServices>
   let appContainer: TestContainer
   let assetService: AssetService
-  let tigerbeetleContainer: StartedTestContainer
 
   beforeAll(async (): Promise<void> => {
-    const { container, port } = await startTigerbeetleContainer()
-    tigerbeetleContainer = container
+    const { port } = await startTigerbeetleContainer()
     Config.tigerbeetleReplicaAddresses = [port]
 
     deps = await initIocContainer(Config)
@@ -39,8 +36,6 @@ describe('Asset Service', (): void => {
 
   afterAll(async (): Promise<void> => {
     await appContainer.shutdown()
-    await new Promise((f) => setTimeout(f, 5000))
-    await tigerbeetleContainer.stop()
   })
 
   describe('create', (): void => {
