bb9df9064e1eedd6157fa3d8816b731dd9e81b1e
Sabine Schaller <sabine@coil.com>
2023-02-09 10:43:04 +0100

    test(backend): update error message

--- packages/backend/src/accounting/tigerbeetle/errors.ts
+++ packages/backend/src/accounting/tigerbeetle/errors.ts
@@ -6,7 +6,7 @@ import { AccountId } from './utils'
 
 export class TigerbeetleCreateAccountError extends Error {
   constructor(public code: number) {
-    super(`CreateAccountError code: ${code}`)
+    super(`Tigerbeetle error code: ${code}`)
     this.name = 'CreateAccountError'
   }
 }
