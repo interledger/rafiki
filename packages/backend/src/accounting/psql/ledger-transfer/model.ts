43fe3e28930639586ef0c561238804e8f95245de
Max Kurapov <max@interledger.org>
2023-02-10 02:12:40 +0100

    feat(backend): adding LedgerTransfer model & service

---
+++ packages/backend/src/accounting/psql/ledger-transfer/model.ts
@@ -0,0 +1,66 @@
+import { Model } from 'objection'
+import { Asset } from '../../../asset/model'
+import { BaseModel } from '../../../shared/baseModel'
+import { LedgerAccount } from '../ledger-account/model'
+
+export enum LedgerTransferState {
+  PENDING = 'PENDING',
+  POSTED = 'POSTED',
+  VOIDED = 'VOIDED'
+}
+
+export enum LedgerTransferType {
+  WITHDRAWAL = 'WITHDRAWAL',
+  DEPOSIT = 'DEPOSIT'
+}
+
+export class LedgerTransfer extends BaseModel {
+  public static get tableName(): string {
+    return 'ledgerTransfers'
+  }
+
+  public readonly id!: string
+
+  public readonly creditAccountId!: string
+  public readonly creditAccount?: LedgerAccount
+  public readonly debitAccountId!: string
+  public readonly debitAccount?: LedgerAccount
+
+  public readonly transferRef!: string
+
+  public readonly amount!: bigint
+
+  public readonly assetId!: string
+  public readonly asset?: Asset
+
+  public readonly expiresAt?: Date
+  public readonly state!: LedgerTransferState
+  public readonly type?: LedgerTransferType
+
+  static relationMappings = {
+    asset: {
+      relation: Model.HasOneRelation,
+      modelClass: Asset,
+      join: {
+        from: 'ledgerTransfers.assetId',
+        to: 'assets.id'
+      }
+    },
+    creditAccount: {
+      relation: Model.HasOneRelation,
+      modelClass: LedgerAccount,
+      join: {
+        from: 'ledgerTransfers.assetId',
+        to: 'ledgerAccounts.id'
+      }
+    },
+    debitAccount: {
+      relation: Model.HasOneRelation,
+      modelClass: LedgerAccount,
+      join: {
+        from: 'ledgerTransfers.assetId',
+        to: 'ledgerAccounts.id'
+      }
+    }
+  }
+}
---
+++ packages/backend/src/accounting/psql/ledger-transfer/service.test.ts
@@ -0,0 +1,153 @@
+import { Knex } from 'knex'
+
+import { createTestApp, TestContainer } from '../../../tests/app'
+import { Config } from '../../../config/app'
+import { IocContract } from '@adonisjs/fold'
+import { initIocContainer } from '../../..'
+import { AppServices } from '../../../app'
+import { Asset } from '../../../asset/model'
+import { randomAsset } from '../../../tests/asset'
+import { truncateTables } from '../../../tests/tableManager'
+import { LedgerAccount } from '../ledger-account/model'
+import { LedgerTransferService } from './service'
+import { LedgerTransferState } from './model'
+import { createLedgerAccount } from '../../../tests/ledgerAccount'
+import { createLedgerTransfer } from '../../../tests/ledgerTransfer'
+
+describe('Ledger Transfer Service', (): void => {
+  let deps: IocContract<AppServices>
+  let appContainer: TestContainer
+  let knex: Knex
+  let asset: Asset
+  let ledgerTransferService: LedgerTransferService
+
+  beforeAll(async (): Promise<void> => {
+    deps = initIocContainer({ ...Config, useTigerbeetle: false })
+    appContainer = await createTestApp(deps)
+    ledgerTransferService = await deps.use('ledgerTransferService')
+    knex = appContainer.knex
+  })
+
+  let creditAccount: LedgerAccount
+  let debitAccount: LedgerAccount
+
+  beforeEach(async (): Promise<void> => {
+    asset = await Asset.query(knex).insertAndFetch(randomAsset())
+    ;[creditAccount, debitAccount] = await Promise.all([
+      createLedgerAccount({ assetId: asset.id }, knex),
+      createLedgerAccount({ assetId: asset.id }, knex)
+    ])
+  })
+
+  afterEach(async (): Promise<void> => {
+    jest.useRealTimers()
+    await truncateTables(knex)
+  })
+
+  afterAll(async (): Promise<void> => {
+    await appContainer.shutdown()
+  })
+
+  describe('getAccountTransfers', (): void => {
+    test.each`
+      accountType
+      ${'credit'}
+      ${'debit'}
+    `(
+      `gets POSTED transfer for $accountType account `,
+      async ({ accountType }): Promise<void> => {
+        const transfer = await createLedgerTransfer(
+          {
+            assetId: creditAccount.assetId,
+            creditAccountId: creditAccount.id,
+            debitAccountId: debitAccount.id,
+            state: LedgerTransferState.POSTED
+          },
+          knex
+        )
+
+        await expect(
+          ledgerTransferService.getAccountTransfers(
+            accountType === 'credit' ? creditAccount.id : debitAccount.id
+          )
+        ).resolves.toEqual([transfer])
+      }
+    )
+
+    test.each`
+      accountType
+      ${'credit'}
+      ${'debit'}
+    `(
+      `gets PENDING transfer for $accountType account `,
+      async ({ accountType }): Promise<void> => {
+        const transfer = await createLedgerTransfer(
+          {
+            assetId: creditAccount.assetId,
+            creditAccountId: creditAccount.id,
+            debitAccountId: debitAccount.id,
+            state: LedgerTransferState.PENDING
+          },
+          knex
+        )
+
+        await expect(
+          ledgerTransferService.getAccountTransfers(
+            accountType === 'credit' ? creditAccount.id : debitAccount.id
+          )
+        ).resolves.toEqual([transfer])
+      }
+    )
+
+    test.each`
+      accountType
+      ${'credit'}
+      ${'debit'}
+    `(
+      `ignores expired transfer for $accountType account `,
+      async ({ accountType }): Promise<void> => {
+        await createLedgerTransfer(
+          {
+            assetId: creditAccount.assetId,
+            creditAccountId: creditAccount.id,
+            debitAccountId: debitAccount.id,
+            state: LedgerTransferState.PENDING,
+            expiresAt: new Date(Date.now() - 10)
+          },
+          knex
+        )
+
+        await expect(
+          ledgerTransferService.getAccountTransfers(
+            accountType === 'credit' ? creditAccount.id : debitAccount.id
+          )
+        ).resolves.toEqual([])
+      }
+    )
+
+    test.each`
+      accountType
+      ${'credit'}
+      ${'debit'}
+    `(
+      `ignores VOIDED transfer for $accountType account `,
+      async ({ accountType }): Promise<void> => {
+        await createLedgerTransfer(
+          {
+            assetId: creditAccount.assetId,
+            creditAccountId: creditAccount.id,
+            debitAccountId: debitAccount.id,
+            state: LedgerTransferState.VOIDED
+          },
+          knex
+        )
+
+        await expect(
+          ledgerTransferService.getAccountTransfers(
+            accountType === 'credit' ? creditAccount.id : debitAccount.id
+          )
+        ).resolves.toEqual([])
+      }
+    )
+  })
+})
