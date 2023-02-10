43a92a935c8d055026e32b9f72e24b03c1de359f
Max Kurapov <max@interledger.org>
2023-02-10 02:13:59 +0100

    chore(auth): use trx for token rotation (#1073)
    * chore(auth): use trx for token rotation
    
    * feat(auth): add grant locking
    
    * chore(auth): update var name
    
    * chore(auth): clean up test logging
    
    * chore(auth): fix final test
    
    * chore(auth): add warn if could not find grant for locking
    
    * feat(auth): store accessToken on tokenHttpsigMiddleware
    
    * feat(auth): use ctx.accessToken for management routes
    
    * fix(auth): don't log in revoke route if can't revoke token
    
    * chore(auth): revert v4 <-> uuid change
    
    * fix(auth): update introspect test
    
    * refactor(auth): update accessToken routes & service method definitions
    
    * feat(auth): simplify accessToken methods

--- packages/auth/src/grant/service.test.ts
+++ packages/auth/src/grant/service.test.ts
@@ -22,9 +22,10 @@ describe('Grant Service', (): void => {
   let grant: Grant
 
   beforeAll(async (): Promise<void> => {
-    deps = await initIocContainer(Config)
-    grantService = await deps.use('grantService')
+    deps = initIocContainer(Config)
     appContainer = await createTestApp(deps)
+
+    grantService = await deps.use('grantService')
   })
 
   const CLIENT = faker.internet.url()
@@ -245,6 +246,37 @@ describe('Grant Service', (): void => {
 
     test('Can "delete" unknown grant', async (): Promise<void> => {
       await expect(grantService.deleteGrant(v4())).resolves.toEqual(false)
+    })
+  })
+
+  describe('lock', (): void => {
+    test('a grant reference can be locked', async (): Promise<void> => {
+      const grantRequest: GrantRequest = {
+        ...BASE_GRANT_REQUEST,
+        access_token: {
+          access: [
+            {
+              ...BASE_GRANT_ACCESS,
+              type: AccessType.IncomingPayment
+            }
+          ]
+        }
+      }
+
+      const grant = await grantService.create(grantRequest)
+
+      const timeoutMs = 50
+
+      const lock = async (): Promise<void> => {
+        return await Grant.transaction(async (trx) => {
+          await grantService.lock(grant.id, trx, timeoutMs)
+          await new Promise((resolve) => setTimeout(resolve, timeoutMs + 10))
+          await grantService.get(grant.id)
+        })
+      }
+      await expect(Promise.all([lock(), lock()])).rejects.toThrowError(
+        /Defined query timeout/
+      )
     })
   })
 })
