66085594cb84a6a9c627c08683c0003dfc995032
Max Kurapov <max@interledger.org>
2023-02-20 16:11:10 +0100

    test(backend): remove TB container start & stop from setup & teardown

--- packages/backend/jest.teardown.js
+++ packages/backend/jest.teardown.js
@@ -7,9 +7,6 @@ module.exports = async () => {
   if (global.__BACKEND_POSTGRES__) {
     await global.__BACKEND_POSTGRES__.stop()
   }
-  if (global.__BACKEND_TIGERBEETLE__) {
-    await global.__BACKEND_TIGERBEETLE__.stop()
-  }
   if (global.__BACKEND_REDIS__) {
     await global.__BACKEND_REDIS__.stop()
   }
