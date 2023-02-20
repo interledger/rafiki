02393ab7665899220f1326b79296b353b9931e94
Max Kurapov <max@interledger.org>
2023-02-20 01:04:48 +0100

    fix(backend): fix lint

--- packages/backend/src/accounting/psql/ledger-transfer/index.ts
+++ packages/backend/src/accounting/psql/ledger-transfer/index.ts
@@ -1,4 +1,4 @@
-import { Transaction, TransactionOrKnex, UniqueViolationError } from 'objection'
+import { Transaction, TransactionOrKnex } from 'objection'
 import { LedgerTransfer, LedgerTransferState } from './model'
 import { ServiceDependencies } from '../service'
 import { LedgerAccount } from '../ledger-account/model'
