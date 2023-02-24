85d1c6212eea545bde8e856c157cb8aa960e34b6
Radu-Cristian Popa <radu.popa@breakpointit.eu>
2023-02-24 12:14:16 +0200

    feat(backend): add http endpoint validation for peers (#1163)
    * feat(backend): add http endpoint validation
    
    * feat(backend): add test
    
    * feat(backend): validate http endpoint on peer update
    
    * feat(backend): update peer resolver tests
    
    * test(backend): add successfull `https:` test

---
+++ packages/backend/src/shared/utils.test.ts
@@ -0,0 +1,23 @@
+import { isValidHttpUrl } from './utils'
+
+describe('utils', (): void => {
+  describe('isValidHttpUrl', (): void => {
+    test.each`
+      url                         | result   | type
+      ${''}                       | ${false} | ${'invalid'}
+      ${undefined}                | ${false} | ${'invalid'}
+      ${{}}                       | ${false} | ${'invalid'}
+      ${'mailto:john@doe.com'}    | ${false} | ${'invalid'}
+      ${'ftp://0.0.0.0@0.0.0.0'}  | ${false} | ${'invalid'}
+      ${'javascript:void(0)'}     | ${false} | ${'invalid'}
+      ${'http://'}                | ${false} | ${'invalid'}
+      ${'HTTP://.com'}            | ${false} | ${'invalid'}
+      ${'http://foo'}             | ${true}  | ${'valid'}
+      ${'http://foo.bar.baz.com'} | ${true}  | ${'valid'}
+      ${'http://peer.test:3000'}  | ${true}  | ${'valid'}
+      ${'https://foo'}            | ${true}  | ${'valid'}
+    `('returns $result for $type HTTP url', ({ url, result }): void => {
+      expect(isValidHttpUrl(url)).toEqual(result)
+    })
+  })
+})
