d0dc7f7b3288cd47622459f705d7af2d1aaf1aed
Sabine Schaller <sabine@coil.com>
2023-03-03 08:35:34 +0100

    feat(localenv): organize into directories

---
+++ infrastructure/local/cloud-nine-wallet/dbinit.sql
@@ -0,0 +1,15 @@
+CREATE USER cloud_nine_wallet_backend WITH PASSWORD 'cloud_nine_wallet_backend';
+CREATE DATABASE cloud_nine_wallet_backend;
+ALTER DATABASE cloud_nine_wallet_backend OWNER TO cloud_nine_wallet_backend;
+
+CREATE USER cloud_nine_wallet_auth WITH PASSWORD 'cloud_nine_wallet_auth';
+CREATE DATABASE cloud_nine_wallet_auth;
+ALTER DATABASE cloud_nine_wallet_auth OWNER TO cloud_nine_wallet_auth;
+
+CREATE USER happy_life_bank_backend WITH PASSWORD 'happy_life_bank_backend';
+CREATE DATABASE happy_life_bank_backend;
+ALTER DATABASE happy_life_bank_backend OWNER TO happy_life_bank_backend;
+
+CREATE USER happy_life_bank_auth WITH PASSWORD 'happy_life_bank_auth';
+CREATE DATABASE happy_life_bank_auth;
+ALTER DATABASE happy_life_bank_auth OWNER TO happy_life_bank_auth;
