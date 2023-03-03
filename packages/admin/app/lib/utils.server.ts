a061c24802d7dfd00d286bd9643a96cd2d5eaa08
Radu-Cristian Popa <radu.popa@breakpointit.eu>
2023-03-03 10:11:31 +0200

    chore: remove dead code

--- packages/admin/app/lib/utils.server.ts
+++
@@ -1,8 +0,0 @@
-// Obscures the auth token in the loader before sending it to the frontend
-export function obscureAuthToken(token: string): string {
-  if (token.length < 5) {
-    return token.replace(/./g, '*')
-  } else {
-    return token.replace(/(?<=...)./g, '*')
-  }
-}
