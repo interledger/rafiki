25feefb5c7e362a752216d0d2a9124d840537198
Radu-Cristian Popa <radu.popa@breakpointit.eu>
2023-03-13 15:42:34 +0200

    chore: add apollo back to global namespace

--- packages/frontend/app/lib/apollo.server.ts
+++ packages/frontend/app/lib/apollo.server.ts
@@ -1,7 +1,10 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
+import type { NormalizedCacheObject } from '@apollo/client'
 
 /* eslint-disable no-var */
 declare global {
+  var __apolloClient: ApolloClient<NormalizedCacheObject> | undefined
+
   interface BigInt {
     toJSON(): string
   }
@@ -13,17 +16,20 @@ BigInt.prototype.toJSON = function (this: bigint) {
   return this.toString()
 }
 
-const apolloClient = new ApolloClient({
-  cache: new InMemoryCache({}),
-  defaultOptions: {
-    query: {
-      fetchPolicy: 'no-cache'
+if (!global.__apolloClient) {
+  global.__apolloClient = new ApolloClient({
+    cache: new InMemoryCache({}),
+    defaultOptions: {
+      query: {
+        fetchPolicy: 'no-cache'
+      },
+      mutate: {
+        fetchPolicy: 'no-cache'
+      }
     },
-    mutate: {
-      fetchPolicy: 'no-cache'
-    }
-  },
-  uri: process.env.GRAPHQL_URL ?? 'http://localhost:3001/graphql'
-})
+    uri: process.env.GRAPHQL_URL ?? 'http://localhost:3001/graphql'
+  })
+}
+const apolloClient = global.__apolloClient
 
 export { apolloClient }
