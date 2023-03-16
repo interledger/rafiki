97cc040f31a007214e98c0e257eb171ce9c9445b
Max Kurapov <max@interledger.org>
2023-03-16 16:29:02 +0100

    feat(open-payments): add return types to new functions

--- packages/open-payments/src/openapi/index.ts
+++ packages/open-payments/src/openapi/index.ts
@@ -1,10 +1,10 @@
-import { createOpenAPI } from 'openapi'
+import { createOpenAPI, OpenAPI } from 'openapi'
 import path from 'path'
 
-export async function getResourceServerOpenApi() {
+export async function getResourceServerOpenApi(): Promise<OpenAPI> {
   return createOpenAPI(path.resolve(__dirname, './resource-server.yaml'))
 }
 
-export async function getAuthServerOpenApi() {
+export async function getAuthServerOpenApi(): Promise<OpenAPI> {
   return createOpenAPI(path.resolve(__dirname, './auth-server.yaml'))
 }
