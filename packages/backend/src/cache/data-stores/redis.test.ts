d95e3208c01a49c4230d692ea6ceca26adf47554
Max Kurapov <max@interledger.org>
2023-04-25 13:36:37 +0200

    chore: rearrange tests

--- packages/backend/src/cache/data-stores/redis.test.ts
+++ packages/backend/src/cache/data-stores/redis.test.ts
@@ -24,13 +24,6 @@ describe('Redis Data Store', (): void => {
       await expect(dataStore.set('foo', 'bar')).resolves.toBe(true)
     })
 
-    test('keys expire properly', async () => {
-      await expect(dataStore.set('foo', 'bar')).resolves.toBe(true)
-      await expect(dataStore.get('foo')).resolves.toBe('bar')
-      await new Promise((resolve) => setTimeout(resolve, ttlMs))
-      await expect(dataStore.get('foo')).resolves.toBeUndefined()
-    })
-
     test('values can be overriden', async () => {
       await expect(dataStore.set('foo', 'bar')).resolves.toBe(true)
       await expect(dataStore.set('foo', 'barbar')).resolves.toBe(true)
@@ -42,9 +35,17 @@ describe('Redis Data Store', (): void => {
     test('returns undefined if key not set', async () => {
       await expect(dataStore.get('foo')).resolves.toBeUndefined()
     })
+
     test('returns value if key set', async () => {
       await expect(dataStore.set('foo', 'bar')).resolves.toBe(true)
       await expect(dataStore.get('foo')).resolves.toBe('bar')
+    })
+
+    test('keys expire properly', async () => {
+      await expect(dataStore.set('foo', 'bar')).resolves.toBe(true)
+      await expect(dataStore.get('foo')).resolves.toBe('bar')
+      await new Promise((resolve) => setTimeout(resolve, ttlMs))
+      await expect(dataStore.get('foo')).resolves.toBeUndefined()
     })
   })
 
@@ -52,6 +53,7 @@ describe('Redis Data Store', (): void => {
     test('returns false if no key deleted', async () => {
       await expect(dataStore.delete('foo')).resolves.toBe(false)
     })
+
     test('returns true if key was deleted', async () => {
       await expect(dataStore.set('foo', 'bar')).resolves.toBe(true)
       await expect(dataStore.delete('foo')).resolves.toBe(true)
