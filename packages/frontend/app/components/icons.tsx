2410f116217682f208f346a804fd8ebced06d615
Radu-Cristian Popa <radu.popa@breakpointit.eu>
2023-05-17 12:47:02 +0300

    Add Check icon

--- packages/frontend/app/components/icons.tsx
+++ packages/frontend/app/components/icons.tsx
@@ -178,3 +178,22 @@ export const XCircleSolid = (props: SVGProps<SVGSVGElement>) => {
     </svg>
   )
 }
+
+export const Check = (props: SVGProps<SVGSVGElement>) => {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      fill='none'
+      viewBox='0 0 24 24'
+      strokeWidth={1.5}
+      stroke='currentColor'
+        {...props}
+    >
+      <path
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        d='M4.5 12.75l6 6 9-13.5'
+      />
+    </svg>
+  )
+}
