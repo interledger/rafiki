5bb5d45e7871270b85f31e51b798c43dcde88cdb
Blair Currey <12960453+BlairCurrey@users.noreply.github.com>
2023-05-25 10:14:35 -0400

    Update packages/backend/src/tests/webhook.ts
    Co-authored-by: Max Kurapov <max@interledger.org>

--- packages/backend/src/tests/webhook.ts
+++ packages/backend/src/tests/webhook.ts
@@ -8,11 +8,12 @@ import { EventPayload } from '../webhook/service'
 
 export const webhookEventTypes = ['event1', 'event2', 'event3'] as const
 
-export function randomWebhookEvent(): EventPayload {
+export function randomWebhookEvent(overrides?: Partial<EventPayload>): EventPayload {
   return {
     id: uuid(),
     type: sample(webhookEventTypes) as string,
-    data: { field1: faker.string.sample() }
+    data: { field1: faker.string.sample() },
+    ...overrides
   }
 }
 
