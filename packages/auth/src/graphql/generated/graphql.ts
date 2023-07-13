905148fa5ee98140b11247e608872c6e12cfeb07
Blair Currey <12960453+BlairCurrey@users.noreply.github.com>
2023-07-13 11:17:00 -0400

    chore: generate gql

--- packages/auth/src/graphql/generated/graphql.ts
+++ packages/auth/src/graphql/generated/graphql.ts
@@ -4,32 +4,34 @@ export type InputMaybe<T> = T | undefined;
 export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
 export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
 export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+export type MakeEmpty<T extends { [key: string]: unknown }, K extends keyof T> = { [_ in K]?: never };
+export type Incremental<T> = T | { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never };
 export type RequireFields<T, K extends keyof T> = Omit<T, K> & { [P in K]-?: NonNullable<T[P]> };
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
-  ID: string;
-  String: string;
-  Boolean: boolean;
-  Int: number;
-  Float: number;
-  UInt8: any;
-  UInt64: any;
+  ID: { input: string; output: string; }
+  String: { input: string; output: string; }
+  Boolean: { input: boolean; output: boolean; }
+  Int: { input: number; output: number; }
+  Float: { input: number; output: number; }
+  UInt8: { input: any; output: any; }
+  UInt64: { input: any; output: any; }
 };
 
 export type Access = Model & {
   __typename?: 'Access';
   /** Access action (create, read, list or complete) */
-  actions: Array<Maybe<Scalars['String']>>;
+  actions: Array<Maybe<Scalars['String']['output']>>;
   /** Date-time of creation */
-  createdAt: Scalars['String'];
+  createdAt: Scalars['String']['output'];
   /** Access id */
-  id: Scalars['ID'];
+  id: Scalars['ID']['output'];
   /** Payment pointer of a sub-resource (incoming payment, outgoing payment, or quote) */
-  identifier?: Maybe<Scalars['String']>;
+  identifier?: Maybe<Scalars['String']['output']>;
   /** Payment limits */
   limits?: Maybe<LimitData>;
   /** Access type (incoming payment, outgoing payment, or quote) */
-  type: Scalars['String'];
+  type: Scalars['String']['output'];
 };
 
 export type FilterGrantState = {
@@ -37,7 +39,7 @@ export type FilterGrantState = {
 };
 
 export type FilterString = {
-  in: Array<Scalars['String']>;
+  in: Array<Scalars['String']['input']>;
 };
 
 export type Grant = Model & {
@@ -45,18 +47,18 @@ export type Grant = Model & {
   /** Access details */
   access: Array<Access>;
   /** Payment pointer of the grantee's account */
-  client: Scalars['String'];
+  client: Scalars['String']['output'];
   /** Date-time of creation */
-  createdAt: Scalars['String'];
+  createdAt: Scalars['String']['output'];
   /** Grant id */
-  id: Scalars['ID'];
+  id: Scalars['ID']['output'];
   /** State of the grant */
   state: GrantState;
 };
 
 export type GrantEdge = {
   __typename?: 'GrantEdge';
-  cursor: Scalars['String'];
+  cursor: Scalars['String']['output'];
   node: Grant;
 };
 
@@ -85,18 +87,18 @@ export type GrantsConnection = {
 export type LimitData = {
   __typename?: 'LimitData';
   /** Interval between payments */
-  interval?: Maybe<Scalars['String']>;
+  interval?: Maybe<Scalars['String']['output']>;
   /** Amount to receive */
   receiveAmount?: Maybe<PaymentAmount>;
   /** Payment pointer URL of the receiver */
-  receiver?: Maybe<Scalars['String']>;
+  receiver?: Maybe<Scalars['String']['output']>;
   /** Amount to send */
   sendAmount?: Maybe<PaymentAmount>;
 };
 
 export type Model = {
-  createdAt: Scalars['String'];
-  id: Scalars['ID'];
+  createdAt: Scalars['String']['output'];
+  id: Scalars['ID']['output'];
 };
 
 export type Mutation = {
@@ -111,30 +113,30 @@ export type MutationRevokeGrantArgs = {
 };
 
 export type MutationResponse = {
-  code: Scalars['String'];
-  message: Scalars['String'];
-  success: Scalars['Boolean'];
+  code: Scalars['String']['output'];
+  message: Scalars['String']['output'];
+  success: Scalars['Boolean']['output'];
 };
 
 export type PageInfo = {
   __typename?: 'PageInfo';
   /** Paginating forwards: the cursor to continue. */
-  endCursor?: Maybe<Scalars['String']>;
+  endCursor?: Maybe<Scalars['String']['output']>;
   /** Paginating forwards: Are there more pages? */
-  hasNextPage: Scalars['Boolean'];
+  hasNextPage: Scalars['Boolean']['output'];
   /** Paginating backwards: Are there more pages? */
-  hasPreviousPage: Scalars['Boolean'];
+  hasPreviousPage: Scalars['Boolean']['output'];
   /** Paginating backwards: the cursor to continue. */
-  startCursor?: Maybe<Scalars['String']>;
+  startCursor?: Maybe<Scalars['String']['output']>;
 };
 
 export type PaymentAmount = {
   __typename?: 'PaymentAmount';
   /** [ISO 4217 currency code](https://en.wikipedia.org/wiki/ISO_4217), e.g. `USD` */
-  assetCode: Scalars['String'];
+  assetCode: Scalars['String']['output'];
   /** Difference in orders of magnitude between the standard unit of an asset and a corresponding fractional unit */
-  assetScale: Scalars['UInt8'];
-  value: Scalars['UInt64'];
+  assetScale: Scalars['UInt8']['output'];
+  value: Scalars['UInt64']['output'];
 };
 
 export type Query = {
@@ -147,27 +149,27 @@ export type Query = {
 
 
 export type QueryGrantArgs = {
-  id: Scalars['ID'];
+  id: Scalars['ID']['input'];
 };
 
 
 export type QueryGrantsArgs = {
-  after?: InputMaybe<Scalars['String']>;
-  before?: InputMaybe<Scalars['String']>;
+  after?: InputMaybe<Scalars['String']['input']>;
+  before?: InputMaybe<Scalars['String']['input']>;
   filter?: InputMaybe<GrantFilter>;
-  first?: InputMaybe<Scalars['Int']>;
-  last?: InputMaybe<Scalars['Int']>;
+  first?: InputMaybe<Scalars['Int']['input']>;
+  last?: InputMaybe<Scalars['Int']['input']>;
 };
 
 export type RevokeGrantInput = {
-  grantId: Scalars['String'];
+  grantId: Scalars['String']['input'];
 };
 
 export type RevokeGrantMutationResponse = MutationResponse & {
   __typename?: 'RevokeGrantMutationResponse';
-  code: Scalars['String'];
-  message: Scalars['String'];
-  success: Scalars['Boolean'];
+  code: Scalars['String']['output'];
+  message: Scalars['String']['output'];
+  success: Scalars['Boolean']['output'];
 };
 
 
@@ -238,11 +240,16 @@ export type DirectiveResolverFn<TResult = {}, TParent = {}, TContext = {}, TArgs
 ) => TResult | Promise<TResult>;
 
 
+/** Mapping of interface types */
+export type ResolversInterfaceTypes<RefType extends Record<string, unknown>> = {
+  Model: ( Partial<Access> ) | ( Partial<Grant> );
+  MutationResponse: ( Partial<RevokeGrantMutationResponse> );
+};
 
 /** Mapping between all available schema types and the resolvers types */
 export type ResolversTypes = {
   Access: ResolverTypeWrapper<Partial<Access>>;
-  Boolean: ResolverTypeWrapper<Partial<Scalars['Boolean']>>;
+  Boolean: ResolverTypeWrapper<Partial<Scalars['Boolean']['output']>>;
   FilterGrantState: ResolverTypeWrapper<Partial<FilterGrantState>>;
   FilterString: ResolverTypeWrapper<Partial<FilterString>>;
   Grant: ResolverTypeWrapper<Partial<Grant>>;
@@ -250,46 +257,46 @@ export type ResolversTypes = {
   GrantFilter: ResolverTypeWrapper<Partial<GrantFilter>>;
   GrantState: ResolverTypeWrapper<Partial<GrantState>>;
   GrantsConnection: ResolverTypeWrapper<Partial<GrantsConnection>>;
-  ID: ResolverTypeWrapper<Partial<Scalars['ID']>>;
-  Int: ResolverTypeWrapper<Partial<Scalars['Int']>>;
+  ID: ResolverTypeWrapper<Partial<Scalars['ID']['output']>>;
+  Int: ResolverTypeWrapper<Partial<Scalars['Int']['output']>>;
   LimitData: ResolverTypeWrapper<Partial<LimitData>>;
-  Model: ResolversTypes['Access'] | ResolversTypes['Grant'];
+  Model: ResolverTypeWrapper<ResolversInterfaceTypes<ResolversTypes>['Model']>;
   Mutation: ResolverTypeWrapper<{}>;
-  MutationResponse: ResolversTypes['RevokeGrantMutationResponse'];
+  MutationResponse: ResolverTypeWrapper<ResolversInterfaceTypes<ResolversTypes>['MutationResponse']>;
   PageInfo: ResolverTypeWrapper<Partial<PageInfo>>;
   PaymentAmount: ResolverTypeWrapper<Partial<PaymentAmount>>;
   Query: ResolverTypeWrapper<{}>;
   RevokeGrantInput: ResolverTypeWrapper<Partial<RevokeGrantInput>>;
   RevokeGrantMutationResponse: ResolverTypeWrapper<Partial<RevokeGrantMutationResponse>>;
-  String: ResolverTypeWrapper<Partial<Scalars['String']>>;
-  UInt8: ResolverTypeWrapper<Partial<Scalars['UInt8']>>;
-  UInt64: ResolverTypeWrapper<Partial<Scalars['UInt64']>>;
+  String: ResolverTypeWrapper<Partial<Scalars['String']['output']>>;
+  UInt8: ResolverTypeWrapper<Partial<Scalars['UInt8']['output']>>;
+  UInt64: ResolverTypeWrapper<Partial<Scalars['UInt64']['output']>>;
 };
 
 /** Mapping between all available schema types and the resolvers parents */
 export type ResolversParentTypes = {
   Access: Partial<Access>;
-  Boolean: Partial<Scalars['Boolean']>;
+  Boolean: Partial<Scalars['Boolean']['output']>;
   FilterGrantState: Partial<FilterGrantState>;
   FilterString: Partial<FilterString>;
   Grant: Partial<Grant>;
   GrantEdge: Partial<GrantEdge>;
   GrantFilter: Partial<GrantFilter>;
   GrantsConnection: Partial<GrantsConnection>;
-  ID: Partial<Scalars['ID']>;
-  Int: Partial<Scalars['Int']>;
+  ID: Partial<Scalars['ID']['output']>;
+  Int: Partial<Scalars['Int']['output']>;
   LimitData: Partial<LimitData>;
-  Model: ResolversParentTypes['Access'] | ResolversParentTypes['Grant'];
+  Model: ResolversInterfaceTypes<ResolversParentTypes>['Model'];
   Mutation: {};
-  MutationResponse: ResolversParentTypes['RevokeGrantMutationResponse'];
+  MutationResponse: ResolversInterfaceTypes<ResolversParentTypes>['MutationResponse'];
   PageInfo: Partial<PageInfo>;
   PaymentAmount: Partial<PaymentAmount>;
   Query: {};
   RevokeGrantInput: Partial<RevokeGrantInput>;
   RevokeGrantMutationResponse: Partial<RevokeGrantMutationResponse>;
-  String: Partial<Scalars['String']>;
-  UInt8: Partial<Scalars['UInt8']>;
-  UInt64: Partial<Scalars['UInt64']>;
+  String: Partial<Scalars['String']['output']>;
+  UInt8: Partial<Scalars['UInt8']['output']>;
+  UInt64: Partial<Scalars['UInt64']['output']>;
 };
 
 export type AccessResolvers<ContextType = any, ParentType extends ResolversParentTypes['Access'] = ResolversParentTypes['Access']> = {
