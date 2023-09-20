d8ea382035e65bf1b3582719d82f2101d73c9bae
Nathan Lie <lie4nathan@gmail.com>
2023-09-20 04:20:07 -0700

    chore: formatting

--- packages/backend/migrations/20230918113102_rename_payment_pointer_tables.js
+++ packages/backend/migrations/20230918113102_rename_payment_pointer_tables.js
@@ -2,19 +2,19 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
+exports.up = function (knex) {
   return Promise.all([
     knex.schema.renameTable('paymentPointers', 'walletAddresses'),
-  	knex.schema.renameTable('paymentPointerKeys', 'walletAddressKeys'),
+    knex.schema.renameTable('paymentPointerKeys', 'walletAddressKeys'),
     knex.schema.alterTable('walletAddressKeys', function (table) {
       table.renameColumn('paymentPointerId', 'walletAddressId')
       table.foreign('walletAddressId').references('walletAddresses.id')
     }),
-  	knex.schema.alterTable('quotes', function (table) {
-  	  table.renameColumn('paymentPointerId', 'walletAddressId')
-  	  table.foreign('walletAddressId').references('walletAddresses.id')
+    knex.schema.alterTable('quotes', function (table) {
+      table.renameColumn('paymentPointerId', 'walletAddressId')
+      table.foreign('walletAddressId').references('walletAddresses.id')
       table.index(['walletAddressId', 'createdAt', 'id'])
-  	}),
+    }),
     knex.schema.alterTable('incomingPayments', function (table) {
       table.renameColumn('paymentPointerId', 'walletAddressId')
       table.foreign('walletAddressId').references('walletAddresses.id')
@@ -26,25 +26,25 @@ exports.up = function(knex) {
       table.index(['walletAddressId', 'createdAt', 'id'])
     })
   ])
-};
+}
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
+exports.down = function (knex) {
   return Promise.all([
-  	knex.schema.renameTable('walletAddresses', 'paymentPointers'),
-  	knex.schema.renameTable('walletAddressKeys', 'paymentPointerKeys'),
+    knex.schema.renameTable('walletAddresses', 'paymentPointers'),
+    knex.schema.renameTable('walletAddressKeys', 'paymentPointerKeys'),
     knex.schema.alterTable('paymentPointerKeys', function (table) {
       table.renameColumn('walletAddressId', 'paymentPointerId')
       table.foreign('paymentPointerId').references('paymentPointers.id')
     }),
-  	knex.schema.alterTable('quotes', function (table) {
-  	  table.renameColumn('walletAddressId', 'paymentPointerId')
-  	  table.foreign('paymentPointerId').references('paymentPointers.id')
+    knex.schema.alterTable('quotes', function (table) {
+      table.renameColumn('walletAddressId', 'paymentPointerId')
+      table.foreign('paymentPointerId').references('paymentPointers.id')
       table.index(['paymentPointerId', 'createdAt', 'id'])
-  	}),
+    }),
     knex.schema.alterTable('incomingPayments', function (table) {
       table.renameColumn('walletAddressId', 'paymentPointerId')
       table.foreign('paymentPointerId').references('paymentPointers.id')
@@ -54,6 +54,6 @@ exports.down = function(knex) {
       table.renameColumn('walletAddressId', 'paymentPointerId')
       table.foreign('paymentPointerId').references('paymentPointers.id')
       table.index(['paymentPointerId', 'createdAt', 'id'])
-    }),
+    })
   ])
-};
+}
