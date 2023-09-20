451ee967025c309b182b99549b82a53891fd679c
Ömer Talha Özdemir <omer@fynbos.dev>
2023-09-20 16:14:39 +0300

    feat(backend): remove connections route (#1940)
    - remove connections route from open payments spec
    - remove connections route and any associated service functions
    from backend

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -38,7 +38,6 @@ import { createConnectorService } from './connector'
 import { createOpenAPI } from '@interledger/openapi'
 import { createAuthenticatedClient as createOpenPaymentsClient } from '@interledger/open-payments'
 import { createConnectionService } from './open_payments/connection/service'
-import { createConnectionRoutes } from './open_payments/connection/routes'
 import { createPaymentPointerKeyService } from './open_payments/payment_pointer/key/service'
 import { createReceiverService } from './open_payments/receiver/service'
 import { createRemoteIncomingPaymentService } from './open_payments/payment/incoming_remote/service'
@@ -288,13 +287,6 @@ export function initIocContainer(
       logger: await deps.use('logger'),
       openPaymentsUrl: config.openPaymentsUrl,
       streamServer: await deps.use('streamServer')
-    })
-  })
-  container.singleton('connectionRoutes', async (deps) => {
-    return createConnectionRoutes({
-      logger: await deps.use('logger'),
-      incomingPaymentService: await deps.use('incomingPaymentService'),
-      connectionService: await deps.use('connectionService')
     })
   })
   container.singleton('receiverService', async (deps) => {
