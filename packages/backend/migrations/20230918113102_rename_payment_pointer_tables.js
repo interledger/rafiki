5113efd6f5bfc1d716ce2b5f376deff03f4d19c8
Nathan Lie <lie4nathan@gmail.com>
2023-10-19 14:20:21 -0700

    fix: migration typos

--- packages/backend/migrations/20230918113102_rename_payment_pointer_tables.js
+++ packages/backend/migrations/20230918113102_rename_payment_pointer_tables.js
@@ -40,27 +40,21 @@ exports.up = function (knex) {
       table.dropIndex(['paymentPointerId', 'createdAt', 'id'])
       table.renameColumn('paymentPointerId', 'walletAddressId')
       table.foreign('walletAddressId').references('walletAddresses.id')
-      table.index(['walletAddressId'])
-      table.index(['createdAt'])
-      table.index(['id'])
+      table.index(['walletAddressId', 'createdAt', 'id'])
     }),
     knex.schema.alterTable('incomingPayments', function (table) {
       table.dropForeign(['paymentPointerId'])
       table.dropIndex(['paymentPointerId', 'createdAt', 'id'])
       table.renameColumn('paymentPointerId', 'walletAddressId')
       table.foreign('walletAddressId').references('walletAddresses.id')
-      table.index(['walletAddressId'])
-      table.index(['createdAt'])
-      table.index(['id'])
+      table.index(['walletAddressId', 'createdAt', 'id'])
     }),
     knex.schema.alterTable('outgoingPayments', function (table) {
       table.dropForeign(['paymentPointerId'])
       table.dropIndex(['paymentPointerId', 'createdAt', 'id'])
       table.renameColumn('paymentPointerId', 'walletAddressId')
       table.foreign('walletAddressId').references('walletAddresses.id')
-      table.index(['walletAddressId'])
-      table.index(['createdAt'])
-      table.index(['id'])
+      table.index(['walletAddressId', 'createdAt', 'id'])
     }),
     knex('webhookEvents')
       .update({
@@ -126,7 +120,6 @@ exports.down = function (knex) {
     ),
     knex.schema.renameTable('walletAddressKeys', 'paymentPointerKeys'),
     knex.schema.alterTable('paymentPointerKeys', function (table) {
-      table.dropForeign(['walletAddressId'])
       table.renameColumn('walletAddressId', 'paymentPointerId')
       table.foreign('paymentPointerId').references('paymentPointers.id')
     }),
