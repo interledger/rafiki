4c21be2833f2471277e5cf5657eb1cc58739a40d
JoblersTune <sarah38186@gmail.com>
2023-10-23 10:04:18 +0200

    chore: moved Welcome to be the first line on the home page

--- packages/frontend/app/routes/_index.tsx
+++ packages/frontend/app/routes/_index.tsx
@@ -3,13 +3,11 @@ import { version } from '../../../../package.json'
 export default function Index() {
   return (
     <div className='pt-4 flex flex-col'>
-      <div
-        className='flex flex-col rounded-md bg-offwhite px-6 text-center min-h-[calc(100vh-3rem)]'
-      >
+      <div className='flex flex-col rounded-md bg-offwhite px-6 text-center min-h-[calc(100vh-3rem)]'>
         <div className='p-10 space-y-16'>
-          <h1 className='text-9xl pt-16 text-[#F37F64]'>Rafiki Admin</h1>
+          <h1 className='text-9xl pt-16 text-[#F37F64]'>Welcome!</h1>
           <div className='space-y-8'>
-            <p className='text-7xl'>Welcome!</p>
+            <p className='text-7xl'>Rafiki Admin</p>
             <p>This is Rafiki&apos;s administrative user interface.</p>
             <p>v{version}</p>
           </div>
