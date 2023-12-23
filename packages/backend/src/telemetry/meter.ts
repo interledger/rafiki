503b6d27ff88cadeba6ef6ba8e0e732ec0774aa7
beniaminmunteanu <beniamin.munteanu@xpsol.ro>
2023-12-23 18:42:09 +0200

    feat(telemetry): extract privacy as a separate module

--- packages/backend/src/telemetry/meter.ts
+++ packages/backend/src/telemetry/meter.ts
@@ -19,9 +19,8 @@ import { BaseService } from '../shared/baseService'
 export interface TelemetryService {
   getOrCreate(name: string, options?: MetricOptions): Counter
   getServiceName(): string | undefined
-  getTelemetryRatesService(): RatesService // Add this line
+  getRatesService(): RatesService // Add this line
   getBaseAssetCode(): string
-  applyPrivacy(rawValue: number): number
 }
 
 interface TelemetryServiceDependencies extends BaseService {
@@ -41,13 +40,13 @@ export function createTelemetryService(
 class TelemetryServiceImpl implements TelemetryService {
   private serviceName: string
   private meterProvider?: MeterProvider
-  private maxBucketSize: number = 10000000
-  private minBucketSize: number = 2500
+  private ratesService: RatesService
 
   private counters = new Map()
   constructor(private deps: TelemetryServiceDependencies) {
     diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG)
     this.serviceName = deps.serviceName
+    this.ratesService = deps.telemetryRatesService
 
     if (
       deps.collectorUrls &&
@@ -101,64 +100,11 @@ class TelemetryServiceImpl implements TelemetryService {
     return this.serviceName
   }
 
-  getTelemetryRatesService(): RatesService {
-    return this.deps.telemetryRatesService
+  getRatesService(): RatesService {
+    return this.ratesService
   }
 
   getBaseAssetCode(): string {
     return this.deps.baseAssetCode
-  }
-
-  private generateLaplaceNoise(scale: number): number {
-    const u = Math.random() - 0.5
-    return -scale * Math.sign(u) * Math.log(1 - 2 * Math.abs(u))
-  }
-
-  private computePrivacyParameter(sensitivity: number): number {
-    return sensitivity * 0.1
-  }
-
-  private getBucketSize(rawValue: number): number {
-    const base = 2
-    const scale = 5000
-    //when this is reached, switch from linear to logarithmic bucket sizing
-    const threshold = 20000
-
-    let bucketSize
-    if (rawValue < threshold) {
-      bucketSize = Math.round(rawValue / scale) * scale
-    } else {
-      bucketSize =
-        Math.pow(base, Math.ceil(Math.log(rawValue / scale) / Math.log(base))) *
-        scale
-    }
-
-    const minBucketSize = this.minBucketSize
-    const maxBucketSize = this.maxBucketSize
-
-    return Math.max(minBucketSize, Math.min(bucketSize, maxBucketSize))
-  }
-
-  private roundValue(rawValue: number, bucketSize: number): number {
-    rawValue = Math.min(rawValue, this.maxBucketSize)
-    const lowerBound = Math.floor(rawValue / bucketSize) * bucketSize
-    const upperBound = Math.ceil(rawValue / bucketSize) * bucketSize
-    const median = (lowerBound + upperBound) / 2
-    const roundedValue = rawValue <= median ? lowerBound : upperBound
-    return Math.max(roundedValue, bucketSize / 2)
-  }
-
-  public applyPrivacy(rawValue: number): number {
-    const bucketSize = this.getBucketSize(rawValue)
-    let roundedValue = this.roundValue(rawValue, bucketSize)
-    const privacyParameter = this.computePrivacyParameter(
-      Math.max(roundedValue / 10, bucketSize)
-    )
-    const laplaceNoise = this.generateLaplaceNoise(privacyParameter)
-    roundedValue += Math.round(laplaceNoise)
-    if (roundedValue === 0) {
-      roundedValue = bucketSize / 2
-    }
-    return roundedValue
   }
 }
