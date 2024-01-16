6139fc96e3d5fcea2698d21971dc266cd9f8dfde
Max Kurapov <max@interledger.org>
2024-01-16 07:23:06 -0700

    chore: fix isbot import

--- packages/frontend/app/entry.server.tsx
+++ packages/frontend/app/entry.server.tsx
@@ -2,7 +2,7 @@ import { PassThrough } from 'stream'
 import type { EntryContext } from '@remix-run/node'
 import { Response } from '@remix-run/node'
 import { RemixServer } from '@remix-run/react'
-import isbot from 'isbot'
+import { isbot } from 'isbot'
 import { renderToPipeableStream } from 'react-dom/server'
 
 const ABORT_DELAY = 5000
