17cc0f6e660cd7a9c1d295ea4040743929b36855
Blair Currey <12960453+BlairCurrey@users.noreply.github.com>
2024-01-17 12:51:05 -0500

    fix(auth): use NonNullable resolver fn types

--- packages/auth/src/graphql/resolvers/grant.ts
+++ packages/auth/src/graphql/resolvers/grant.ts
@@ -11,11 +11,9 @@ import { Pagination } from '../../shared/baseModel'
 import { getPageInfo } from '../../shared/pagination'
 import { Access } from '../../access/model'
 
-export const getGrants: QueryResolvers<ApolloContext>['grants'] = async (
-  _,
-  args,
-  ctx
-): Promise<ResolversTypes['GrantsConnection']> => {
+export const getGrants: NonNullable<
+  QueryResolvers<ApolloContext>['grants']
+> = async (_, args, ctx): Promise<ResolversTypes['GrantsConnection']> => {
   const grantService = await ctx.container.use('grantService')
   const { filter, sortOrder, ...pagination } = args
   const grants = await grantService.getPage(pagination, filter, sortOrder)
@@ -34,11 +32,9 @@ export const getGrants: QueryResolvers<ApolloContext>['grants'] = async (
   }
 }
 
-export const getGrantById: QueryResolvers<ApolloContext>['grant'] = async (
-  _,
-  args,
-  ctx
-): Promise<ResolversTypes['Grant']> => {
+export const getGrantById: NonNullable<
+  QueryResolvers<ApolloContext>['grant']
+> = async (_, args, ctx): Promise<ResolversTypes['Grant']> => {
   const grantService = await ctx.container.use('grantService')
   const grant = await grantService.getByIdWithAccess(args.id)
 
@@ -49,53 +45,54 @@ export const getGrantById: QueryResolvers<ApolloContext>['grant'] = async (
   return grantToGraphql(grant)
 }
 
-export const revokeGrant: MutationResolvers<ApolloContext>['revokeGrant'] =
-  async (
-    _,
-    args,
-    ctx
-  ): Promise<ResolversTypes['RevokeGrantMutationResponse']> => {
-    try {
-      const { grantId } = args.input
-      if (!grantId) {
-        return {
-          code: '401',
-          success: false,
-          message: 'Grant Id is not provided'
-        }
-      }
-
-      const grantService = await ctx.container.use('grantService')
-      const revoked = await grantService.revokeGrant(grantId)
-      if (!revoked) {
-        return {
-          code: '404',
-          success: false,
-          message: 'Revoke grant was not successful'
-        }
-      }
-
+export const revokeGrant: NonNullable<
+  MutationResolvers<ApolloContext>['revokeGrant']
+> = async (
+  _,
+  args,
+  ctx
+): Promise<ResolversTypes['RevokeGrantMutationResponse']> => {
+  try {
+    const { grantId } = args.input
+    if (!grantId) {
       return {
-        code: '200',
-        success: true,
-        message: 'Grant revoked'
-      }
-    } catch (err) {
-      ctx.logger.error(
-        {
-          options: args.input.grantId,
-          err
-        },
-        'error revoking grant'
-      )
-
-      return {
-        code: '500',
-        message: 'Error trying to revoke grant',
-        success: false
+        code: '401',
+        success: false,
+        message: 'Grant Id is not provided'
       }
     }
+
+    const grantService = await ctx.container.use('grantService')
+    const revoked = await grantService.revokeGrant(grantId)
+    if (!revoked) {
+      return {
+        code: '404',
+        success: false,
+        message: 'Revoke grant was not successful'
+      }
+    }
+
+    return {
+      code: '200',
+      success: true,
+      message: 'Grant revoked'
+    }
+  } catch (err) {
+    ctx.logger.error(
+      {
+        options: args.input.grantId,
+        err
+      },
+      'error revoking grant'
+    )
+
+    return {
+      code: '500',
+      message: 'Error trying to revoke grant',
+      success: false
+    }
   }
+}
 
 export const grantToGraphql = (grant: Grant): SchemaGrant => ({
   id: grant.id,
