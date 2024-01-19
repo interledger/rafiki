d45879b484248989c78aee23efec2cf36aabcafe
Blair Currey <12960453+BlairCurrey@users.noreply.github.com>
2024-01-19 12:25:52 -0500

    chore(MASE): upgrade module format to v2

--- localenv/mock-account-servicing-entity/remix.config.js
+++ localenv/mock-account-servicing-entity/remix.config.js
@@ -5,7 +5,18 @@ module.exports = {
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
   // publicPath: "/build/",
-  serverDependenciesToBundle: ['axios'],
+  serverDependenciesToBundle: [
+    'axios',
+    'ts-invariant',
+    '@wry/equality',
+    '@wry/trie',
+    '@wry/caches',
+    '@wry/context',
+    'zen-observable-ts',
+    'optimism',
+    '@apollo/client'
+  ],
+  serverModuleFormat: 'cjs',
   future: {
     v2_routeConvention: true
   }
