2db091cc06f3773a457a0709a7ea533b69cdd3d3
Blair Currey <12960453+BlairCurrey@users.noreply.github.com>
2024-01-24 21:04:17 -0500

    refactor: rework new cookie secure env var

--- packages/frontend/app/lib/message.server.ts
+++ packages/frontend/app/lib/message.server.ts
@@ -1,5 +1,6 @@
 import { type Session, type SessionData, redirect } from '@remix-run/node'
 import { createCookieSessionStorage } from '@remix-run/node'
+import { parseBool } from '~/shared/utils'
 
 const ONE_MINUTE_IN_S = 60
 
@@ -20,10 +21,9 @@ export const messageStorage = createCookieSessionStorage<
     sameSite: 'lax',
     maxAge: ONE_MINUTE_IN_S,
     secrets: ['MY_SUPER_SECRET_TOKEN'],
-    secure:
-      process.env.COOKIE_SECURE === undefined
-        ? true
-        : ['true', 't', '1'].includes(process.env.COOKIE_SECURE.toLowerCase())
+    secure: process.env.ENABLE_INSECURE_MESSAGE_COOKIE
+      ? !parseBool(process.env.ENABLE_INSECURE_MESSAGE_COOKIE)
+      : process.env.NODE_ENV === 'production'
   }
 })
 
