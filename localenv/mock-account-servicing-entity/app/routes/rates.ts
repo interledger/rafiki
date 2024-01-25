eaaee0e21b924be719941ed6be48cc16774aaadc
Blair Currey <12960453+BlairCurrey@users.noreply.github.com>
2024-01-25 14:14:28 -0500

    chore: rm unnecessary eslint ignore

--- localenv/mock-account-servicing-entity/app/routes/rates.ts
+++ localenv/mock-account-servicing-entity/app/routes/rates.ts
@@ -2,7 +2,6 @@ import type { LoaderFunctionArgs } from '@remix-run/node'
 import { json } from '@remix-run/node'
 import { CONFIG as config } from '~/lib/parse_config.server'
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function loader({ request }: LoaderFunctionArgs) {
   const base = new URL(request.url).searchParams.get('base') || 'USD'
 
