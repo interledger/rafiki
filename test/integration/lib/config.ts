ec4fe8fc4fd59971b25ecca3ef53db12aafe589b
Blair Currey <12960453+BlairCurrey@users.noreply.github.com>
2024-02-05 11:38:34 -0500

    refactor: move testenv configuration into new dir

--- test/integration/lib/config.ts
+++ test/integration/lib/config.ts
@@ -5,30 +5,30 @@ import type { Config } from 'mock-account-servicing-lib'
 
 import { resolve } from 'path'
 
+const C9_KEY_PATH = resolve(
+  __dirname,
+  '../testenv/cloud-nine-wallet/private-key.pem'
+)
+const C9_SEED_PATH = resolve(__dirname, '../testenv/cloud-nine-wallet/seed.yml')
+
 export const c9Config: Config = {
-  seed: parse(
-    readFileSync(
-      resolve(__dirname, '../seed.cloud-nine-wallet-test.yml')
-    ).toString('utf8')
-  ),
-  key: loadOrGenerateKey(
-    resolve(__dirname, '../private-key.cloud-nine-wallet-test.pem')
-  ),
+  seed: parse(readFileSync(C9_SEED_PATH).toString('utf8')),
+  key: loadOrGenerateKey(C9_KEY_PATH),
   publicHost: 'https://cloud-nine-wallet-backend-test',
   testnetAutoPeerUrl: '',
   authServerDomain: 'http://localhost:3006',
   graphqlUrl: 'http://localhost:3001/graphql'
 } as const
 
+const HLB_KEY_PATH = resolve(
+  __dirname,
+  '../testenv/happy-life-bank/private-key.pem'
+)
+const HLB_SEED_PATH = resolve(__dirname, '../testenv/happy-life-bank/seed.yml')
+
 export const hlbConfig: Config = {
-  seed: parse(
-    readFileSync(
-      resolve(__dirname, '../seed.happy-life-bank-test.yml')
-    ).toString('utf8')
-  ),
-  key: loadOrGenerateKey(
-    resolve(__dirname, '../private-key.happy-life-bank-test.pem')
-  ),
+  seed: parse(readFileSync(HLB_SEED_PATH).toString('utf8')),
+  key: loadOrGenerateKey(HLB_KEY_PATH),
   publicHost: 'https://happy-life-bank-backend-test',
   testnetAutoPeerUrl: '',
   authServerDomain: 'http://localhost:3006',
