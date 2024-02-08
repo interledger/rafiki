f282b993946d3addc5e3ea0f6cf7102b349ae5c5
JoblersTune <sarah38186@gmail.com>
2024-02-08 14:33:26 +0200

    feat: basic Docker setup for Hydra

---
+++ packages/frontend/app/parse_config.server.ts
@@ -0,0 +1,20 @@
+import { parse } from 'yaml'
+import { readFileSync } from 'fs'
+
+export interface Client {
+  id: string
+  name: string
+  redirectUri: string
+}
+
+export interface Config {
+  clients: Array<Client>
+}
+
+export const CONFIG: Config = {
+  ...parse(
+    readFileSync(
+      process.env.SEED_FILE_LOCATION || `./seed.example.yml`
+    ).toString('utf8')
+  )
+}
