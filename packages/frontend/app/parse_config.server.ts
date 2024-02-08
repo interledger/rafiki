8012ffa38119bcadf6c91b5cccd83c2829b0c7d5
JoblersTune <sarah38186@gmail.com>
2024-02-08 15:07:29 +0200

    chore: improving the Hydra seeding

--- packages/frontend/app/parse_config.server.ts
+++ packages/frontend/app/parse_config.server.ts
@@ -11,10 +11,8 @@ export interface Config {
   clients: Array<Client>
 }
 
-export const CONFIG: Config = {
-  ...parse(
-    readFileSync(
-      process.env.SEED_FILE_LOCATION || `./seed.example.yml`
-    ).toString('utf8')
-  )
-}
+export const CONFIG: Config = parse(
+  readFileSync(
+    process.env.SEED_FILE_LOCATION || `./seed.example.yml`
+  ).toString('utf8')
+)
