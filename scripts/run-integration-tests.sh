366d02f7108f3fda419d4341de3dfd6bc3f3d678
Blair Currey <12960453+BlairCurrey@users.noreply.github.com>
2024-03-13 13:56:40 -0400

    refactor: use docker healthcheck for running tests

--- scripts/run-integration-tests.sh
+++ scripts/run-integration-tests.sh
@@ -6,7 +6,6 @@
 #   ./script.sh            # Run the script with default options
 #   ./script.sh --build    # Re-build the docker images (-b or --build)
 
-timeout=30
 log_file="/tmp/rafiki_integration_logs.txt"
 build_flag=""
 
@@ -25,34 +24,7 @@ while [[ $# -gt 0 ]]; do
 done
 
 pnpm --filter integration testenv:compose down --volumes
-pnpm --filter integration testenv:compose up -d $build_flag
+pnpm --filter integration testenv:compose up -d --wait $build_flag
 pnpm --filter integration testenv:compose logs -f --tail=0 > "$log_file" 2>&1 &
-
-# perform health check until OK, or errors if timeout.
-function pollUrl() {
-  local url="$1"
-  local start_time=$(date +%s)
-
-  while true; do
-    response=$(curl -s -o /dev/null -w "%{http_code}" "$url")
-
-    if [ "$response" == "200" ]; then
-      echo "Health check successful! Response: OK"
-      break
-    fi
-
-    current_time=$(date +%s)
-    elapsed_time=$((current_time - start_time))
-
-    if [ "$elapsed_time" -ge "$timeout" ]; then
-      echo "Timeout reached. Health check failed."
-      exit 1
-    fi
-
-    sleep 1
-  done
-}
-
-pollUrl "http://localhost:4000/healthz"
 pnpm --filter integration test
 pnpm --filter integration testenv:compose down --volumes
