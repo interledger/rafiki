07ec4fdc76185d69a8762088f92617aad0fa1fc9
Blair Currey <12960453+BlairCurrey@users.noreply.github.com>
2024-03-19 15:47:29 -0400

    feat: auto edit host file

--- test/integration/scripts/run-tests.sh
+++ test/integration/scripts/run-tests.sh
@@ -23,16 +23,30 @@ while [[ $# -gt 0 ]]; do
   esac
 done
 
+# setup hosts
+addHost() {
+  local hostname="$1"
+  
+  # check first to avoid sudo prompt if host is already set
+  if hostile list | grep -q "127.0.0.1 $hostname"; then
+    echo "$hostname already set"
+  else
+    sudo hostile set 127.0.0.1 "$hostname"
+  fi
+}
+addHost "cloud-nine-wallet-test-backend"
+addHost "happy-life-bank-test-backend"
+
+# idempotent start
 pnpm --filter integration testenv:compose down --volumes
 pnpm --filter integration testenv:compose up -d --wait $build_flag
-
 if [ $? -ne 0 ]; then
   echo "Error: Failed to start containers."
   exit 1
 fi
 
+# run tests
 mkdir -p ./tmp
 pnpm --filter integration testenv:compose logs -f > "$log_file" 2>&1 &
-
 pnpm --filter integration test
 pnpm --filter integration testenv:compose down --volumes
