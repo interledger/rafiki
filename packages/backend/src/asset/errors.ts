e2555bf91dfe79a6cb8d3e2cd74de6c10119ad11
Sabine Schaller <sabine@interledger.org>
2024-04-17 10:59:50 +0200

    feat: further simplify asset resolver

--- packages/backend/src/asset/errors.ts
+++ packages/backend/src/asset/errors.ts
@@ -1,3 +1,5 @@
+import { GraphQLErrorCode } from '../graphql/errors'
+
 export enum AssetError {
   DuplicateAsset = 'DuplicateAsset',
   UnknownAsset = 'UnknownAsset',
@@ -9,11 +11,11 @@ export const isAssetError = (o: any): o is AssetError =>
   Object.values(AssetError).includes(o)
 
 export const errorToCode: {
-  [key in AssetError]: number
+  [key in AssetError]: string
 } = {
-  [AssetError.UnknownAsset]: 404,
-  [AssetError.DuplicateAsset]: 400,
-  [AssetError.CannotDeleteInUseAsset]: 400
+  [AssetError.UnknownAsset]: GraphQLErrorCode.NotFound,
+  [AssetError.DuplicateAsset]: GraphQLErrorCode.Duplicate,
+  [AssetError.CannotDeleteInUseAsset]: GraphQLErrorCode.Forbidden
 }
 
 export const errorToMessage: {
