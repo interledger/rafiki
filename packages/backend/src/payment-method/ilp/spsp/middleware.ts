1b22bc88f3e64b3b7a37ac2c877a4edd216a92e9
Max Kurapov <max@interledger.org>
2024-05-13 21:25:10 +0200

    chore(backend): update spsp middleware to use WalletAddressUrlContext

--- packages/backend/src/payment-method/ilp/spsp/middleware.ts
+++ packages/backend/src/payment-method/ilp/spsp/middleware.ts
@@ -1,6 +1,9 @@
 import { WalletAddressContext, SPSPContext } from '../../../app'
 
-export type SPSPWalletAddressContext = WalletAddressContext & SPSPContext
+export type SPSPWalletAddressContext = WalletAddressContext &
+  SPSPContext & {
+    walletAddressUrl: string
+  }
 
 export class SPSPRouteError extends Error {
   public status: number
@@ -36,10 +39,20 @@ const spspMiddleware = async (
   next: () => Promise<unknown>
 ): Promise<void> => {
   if (ctx.accepts('application/spsp4+json')) {
-    ctx.paymentTag = ctx.walletAddress.id
+    const walletAddressService = await ctx.container.use('walletAddressService')
+
+    const walletAddress = await walletAddressService.getByUrl(
+      ctx.walletAddressUrl
+    )
+
+    if (!walletAddress?.isActive) {
+      throw new SPSPRouteError(404, 'Could not get wallet address')
+    }
+
+    ctx.paymentTag = walletAddress.id
     ctx.asset = {
-      code: ctx.walletAddress.asset.code,
-      scale: ctx.walletAddress.asset.scale
+      code: walletAddress.asset.code,
+      scale: walletAddress.asset.scale
     }
     const spspRoutes = await ctx.container.use('spspRoutes')
     await spspRoutes.get(ctx)
