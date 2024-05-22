b7a3652294f16d143262c13ad5aa1ff7ea9c8d73
Arpi <arpad.lengyel@breakpointit.eu>
2024-05-22 11:58:24 +0300

    account creation & update

--- localenv/mock-account-servicing-entity/app/routes/accounts.$accountId.tsx
+++ localenv/mock-account-servicing-entity/app/routes/accounts.$accountId.tsx
@@ -1,53 +1,305 @@
-import { LoaderFunctionArgs, json } from '@remix-run/node'
-import { useLoaderData } from '@remix-run/react'
+import {
+  ActionFunctionArgs,
+  json,
+  type LoaderFunctionArgs
+} from '@remix-run/node'
+import {
+  Form,
+  useLoaderData,
+  useActionData,
+  useNavigation
+} from '@remix-run/react'
+import { ChangeEvent, useState } from 'react'
+import {
+  PageHeader,
+  Table,
+  Button,
+  ErrorPanel,
+  Input,
+  Select
+} from '../components'
+import { ListAssetsQuery } from 'generated/graphql'
+import { messageStorage, setMessageAndRedirect } from '~/lib/message.server'
 import { getAccountTransactions } from '../lib/transactions.server'
-import tableStyle from '../styles/table.css'
+import { loadAssets } from '~/lib/asset.server'
+import { updateAccountSchema, addLiquiditySchema } from '~/lib/validate.server'
+import { getOpenPaymentsUrl } from '~/lib/utils'
+import { ZodFieldErrors } from '~/lib/types'
+import { getAccountWithBalance, updateAccount } from '~/lib/accounts.server'
+import { LiquidityConfirmDialog } from '~/components/LiquidityConfirmDialog'
 
-type LoaderData = {
-  transactions: Awaited<ReturnType<typeof getAccountTransactions>>
-}
-
-export const loader = async ({ params }: LoaderFunctionArgs) => {
+export const loader = async ({ request, params }: LoaderFunctionArgs) => {
   const { accountId } = params
 
   if (!accountId) {
     throw json(null, { status: 400, statusText: 'Account not provided.' })
   }
-  return json<LoaderData>({
-    transactions: await getAccountTransactions(accountId)
+
+  const session = await messageStorage.getSession(request.headers.get('cookie'))
+  const account = await getAccountWithBalance(accountId)
+  if (!account?.id) {
+    return setMessageAndRedirect({
+      session,
+      message: {
+        content: 'Account not found',
+        type: 'error'
+      },
+      location: '/'
+    })
+  }
+
+  const transactions = await getAccountTransactions(accountId)
+  const assets: ListAssetsQuery['assets']['edges'] = await loadAssets()
+
+  return json({
+    account,
+    assets,
+    transactions
   })
 }
 
-export default function Transactions() {
-  const { transactions } = useLoaderData() as LoaderData
+export default function EditAccount() {
+  const { account, assets, transactions } = useLoaderData<typeof loader>()
+  const response = useActionData<typeof action>()
+  const { state } = useNavigation()
+
+  const [liquidityModalOpen, setLiquidityModalOpen] = useState(false)
+  const [amountToAdd, setAmountToAdd] = useState(0)
+
+  const isSubmitting = state === 'submitting'
+
+  const onAmountChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setAmountToAdd(Number(e?.target?.value || 0))
+  }
+console.log(response);
   return (
-    <main>
-      <h1>Transactions</h1>
-      <table>
-        <tr>
-          <th>Date</th>
-          <th>Type</th>
-          <th>Metadata</th>
-          <th>Amount</th>
-        </tr>
-        {transactions.map((trx) => (
-          <tr key={trx.id}>
-            <td>{trx.createdAt}</td>
-            <td>{trx.type}</td>
-            <td>
-              <code>{JSON.stringify(trx.metadata)}</code>
-            </td>
-            <td>
-              {(Number(trx.amountValue) / 100).toFixed(trx.assetScale)}{' '}
-              {trx.assetCode}
-            </td>
-          </tr>
-        ))}
-      </table>
-    </main>
+    <>
+      <div className='pt-4 flex flex-col space-y-8'>
+        <div className='flex flex-col rounded-md bg-offwhite px-6'>
+          <PageHeader>
+            <h3 className='text-xl'>Account</h3>
+            <Button aria-label='go back to accounts page' to='/'>
+              Go to accounts page
+            </Button>
+          </PageHeader>
+          {/* Update Account form */}
+          <Form method='post' replace>
+            <div className='px-6 pt-5'>
+              <ErrorPanel errors={response?.errors.general.message} />
+            </div>
+
+            <fieldset disabled={isSubmitting}>
+              <div className='grid grid-cols-1 px-0 py-3 gap-6 md:grid-cols-3 border-b border-pearl'>
+                <div className='col-span-1 pt-3'>
+                  <h3 className='text-lg font-medium'>General Information</h3>
+                </div>
+                <div className='md:col-span-2 bg-white rounded-md shadow-md'>
+                  <div className='w-full p-4 space-y-3'>
+                    <input type='hidden' name='id' value={account.id} />
+                    <Input
+                      name='name'
+                      label='Account name'
+                      placeholder='Account name'
+                      defaultValue={account?.name}
+                      error={response?.errors.general.fieldErrors.name}
+                    />
+                    <Input
+                      disabled
+                      name='path'
+                      label='Wallet address'
+                      placeholder='jdoe'
+                      value={`${getOpenPaymentsUrl()}/${account?.path}`}
+                    />
+                    <Select
+                      options={assets.map((asset) => ({
+                        value: asset.node.id,
+                        label: `${asset.node.code} (Scale: ${asset.node.scale})`
+                      }))}
+                      name='assetId'
+                      placeholder='Select asset...'
+                      label='Asset'
+                      selectedValue={account?.assetId}
+                      disabled
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className='flex justify-end py-3'>
+                <Button
+                  aria-label='update account'
+                  name='intent'
+                  value='general'
+                  type='submit'
+                >
+                  {isSubmitting ? 'Updating account ...' : 'Update'}
+                </Button>
+              </div>
+            </fieldset>
+          </Form>
+          {/* Update Account form - END */}
+          <fieldset disabled={isSubmitting}>
+            <div className='grid grid-cols-1 px-0 py-3 gap-6 md:grid-cols-3 border-b border-pearl'>
+              <div className='col-span-1 pt-3'>
+                <h3 className='text-lg font-medium'>Balance</h3>
+              </div>
+              <div className='md:col-span-2 bg-white rounded-md shadow-md'>
+                <div className='w-full p-4 space-y-3'>
+                  <p className='font-medium'>Available</p>
+                  <p className='mt-1'>{`${(
+                    Number(account.balance) / 100
+                  ).toFixed(account.assetScale)} ${account.assetCode}`}</p>
+                </div>
+                <div className='w-full p-4 space-y-3'>
+                  <Input
+                    type='number'
+                    name='balance'
+                    label='Amount to add'
+                    placeholder='0'
+                    min={1}
+                    error={response?.errors.addLiquidity.fieldErrors.amount}
+                    onChange={onAmountChange}
+                  />
+                  <div className='flex justify-end'>
+                    <Button
+                      aria-label='add liquidity'
+                      type='button'
+                      disabled={amountToAdd <= 0}
+                      onClick={() => setLiquidityModalOpen(true)}
+                    >
+                      Add
+                    </Button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </fieldset>
+          {/* Transsactions */}
+          <div className='grid grid-cols-1 px-0 py-3 gap-6 md:grid-cols-3 border-b border-pearl'>
+            <div className='col-span-1 pt-3'>
+              <h3 className='text-lg font-medium'>Transactions</h3>
+            </div>
+            <div className='md:col-span-2 bg-white rounded-md shadow-md'>
+              <div className='w-full p-4 space-y-3'>
+                <Table>
+                  <Table.Head
+                    columns={['Date', 'Type', 'Metadata', 'Amount']}
+                  />
+
+                  {transactions.map((trx) => (
+                    <Table.Row key={trx.id}>
+                      <Table.Cell>{trx.createdAt}</Table.Cell>
+                      <Table.Cell>{trx.type}</Table.Cell>
+                      <Table.Cell>
+                        <code>{JSON.stringify(trx.metadata)}</code>
+                      </Table.Cell>
+                      <Table.Cell>
+                        {(Number(trx.amountValue) / 100).toFixed(
+                          trx.assetScale
+                        )}{' '}
+                        {trx.assetCode}
+                      </Table.Cell>
+                    </Table.Row>
+                  ))}
+                </Table>
+                <div className='flex items-center justify-between p-5'>
+                  &nbsp;
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* Transsactions - END */}
+        </div>
+      </div>
+      <LiquidityConfirmDialog
+        title='Confirm adding liquidity'
+        isOpen={liquidityModalOpen}
+        onClose={() => setLiquidityModalOpen(false)}
+        accountId={account.id}
+        displayAmount={amountToAdd.toString()}
+      />
+    </>
   )
 }
 
-export function links() {
-  return [{ rel: 'stylesheet', href: tableStyle }]
+export async function action({ request }: ActionFunctionArgs) {
+  const actionResponse: {
+    errors: {
+      general: {
+        fieldErrors: ZodFieldErrors<typeof updateAccountSchema>
+        message: string[]
+      }
+      addLiquidity: {
+        fieldErrors: ZodFieldErrors<typeof addLiquiditySchema>
+        message: string[]
+      }
+    }
+  } = {
+    errors: {
+      general: {
+        fieldErrors: {},
+        message: []
+      },
+      addLiquidity: {
+        fieldErrors: {},
+        message: []
+      }
+    }
+  }
+
+  const formData = await request.formData()
+  const intent = formData.get('intent')
+  formData.delete('intent')
+
+  const session = await messageStorage.getSession(request.headers.get('cookie'))
+
+  switch (intent) {
+    case 'general': {
+      const result = updateAccountSchema.safeParse(Object.fromEntries(formData))
+
+      if (!result.success) {
+        actionResponse.errors.general.fieldErrors =
+          result.error.flatten().fieldErrors
+        return json({ ...actionResponse }, { status: 400 })
+      }
+
+      const accountId = await updateAccount({
+        ...result.data
+      })
+
+      if (!accountId) {
+        actionResponse.errors.general.message = [
+          'Could not update account. Please try again!'
+        ]
+        return json({ ...actionResponse }, { status: 400 })
+      }
+
+      return setMessageAndRedirect({
+        session,
+        message: {
+          content: 'Account updated.',
+          type: 'success'
+        },
+        location: `/accounts/${accountId}`
+      })
+    }
+    case 'add-liquidity': {
+      const result = addLiquiditySchema.safeParse(Object.fromEntries(formData))
+
+      if (!result.success) {
+        actionResponse.errors.addLiquidity.fieldErrors =
+          result.error.flatten().fieldErrors
+        return json({ ...actionResponse }, { status: 400 })
+      }
+
+      const accountId = ''
+      return setMessageAndRedirect({
+        session,
+        message: {
+          content: 'Liquidity added.',
+          type: 'success'
+        },
+        location: `/accounts/${accountId}`
+      })
+    }
+  }
 }
---
+++ localenv/mock-account-servicing-entity/app/routes/accounts.create.tsx
@@ -0,0 +1,141 @@
+import { json, type ActionFunctionArgs } from '@remix-run/node'
+import {
+  Form,
+  useLoaderData,
+  useActionData,
+  useNavigation
+} from '@remix-run/react'
+
+import { ListAssetsQuery } from 'generated/graphql'
+import { PageHeader, Button, ErrorPanel, Input, Select } from '~/components'
+import { loadAssets } from '~/lib/asset.server'
+import { createAccount } from '~/lib/accounts.server'
+import { createWallet } from '~/lib/wallet.server'
+import { messageStorage, setMessageAndRedirect } from '~/lib/message.server'
+import { createAccountSchema } from '~/lib/validate.server'
+import type { ZodFieldErrors } from '~/lib/types'
+import { getOpenPaymentsUrl } from '~/lib/utils'
+
+export async function loader() {
+  const assets: ListAssetsQuery['assets']['edges'] = await loadAssets()
+
+  return json({ assets })
+}
+
+export default function CreateAccountPage() {
+  const { assets } = useLoaderData<typeof loader>()
+  const response = useActionData<typeof action>()
+  const { state } = useNavigation()
+  const isSubmitting = state === 'submitting'
+
+  return (
+    <div className='pt-4 flex flex-col space-y-4'>
+      <div className='flex flex-col rounded-md bg-offwhite px-6'>
+        <PageHeader>
+          <h3 className='text-xl'>Create Account</h3>
+          <Button aria-label='go back to accounts page' to='/'>
+            Go to accounts page
+          </Button>
+        </PageHeader>
+        {/* Create Account form */}
+        <Form method='post' replace>
+          <div className='px-6 pt-5'>
+            <ErrorPanel errors={response?.errors.message} />
+          </div>
+
+          <fieldset disabled={isSubmitting}>
+            <div className='grid grid-cols-1 px-0 py-3 gap-6 md:grid-cols-3 border-b border-pearl'>
+              <div className='col-span-1 pt-3'>
+                <h3 className='text-lg font-medium'>General Information</h3>
+              </div>
+              <div className='md:col-span-2 bg-white rounded-md shadow-md'>
+                <div className='w-full p-4 space-y-3'>
+                  <Input
+                    name='name'
+                    label='Account name'
+                    placeholder='Account name'
+                    error={response?.errors?.fieldErrors.name}
+                  />
+                  <Input
+                    required
+                    addOn={`${getOpenPaymentsUrl()}/accounts/`}
+                    name='path'
+                    label='Wallet address'
+                    placeholder='jdoe'
+                    error={response?.errors?.fieldErrors.path}
+                  />
+                  <Select
+                    options={assets.map((asset) => ({
+                      value: asset.node.id,
+                      label: `${asset.node.code} (Scale: ${asset.node.scale})`
+                    }))}
+                    error={response?.errors.fieldErrors.assetId}
+                    name='assetId'
+                    placeholder='Select asset...'
+                    label='Asset'
+                    required
+                  />
+                </div>
+              </div>
+            </div>
+            <div className='flex justify-end py-3'>
+              <Button aria-label='create account' type='submit'>
+                {isSubmitting ? 'Creating account ...' : 'Create'}
+              </Button>
+            </div>
+          </fieldset>
+        </Form>
+        {/* Create Account form - END */}
+      </div>
+    </div>
+  )
+}
+
+export async function action({ request }: ActionFunctionArgs) {
+  const errors: {
+    fieldErrors: ZodFieldErrors<typeof createAccountSchema>
+    message: string[]
+  } = {
+    fieldErrors: {},
+    message: []
+  }
+
+  const formData = Object.fromEntries(await request.formData())
+  formData.path = `${getOpenPaymentsUrl()}/accounts/${formData.path}`
+
+  const result = createAccountSchema.safeParse(formData)
+
+  if (!result.success) {
+    errors.fieldErrors = result.error.flatten().fieldErrors
+    return json({ errors }, { status: 400 })
+  }
+
+  const assets: ListAssetsQuery['assets']['edges'] = await loadAssets()
+  const assetDetail = assets.find(
+    (asset) => asset.node.id == result.data.assetId
+  )
+
+  const accountId = await createAccount({
+    ...result.data,
+    assetCode: assetDetail?.node.code || '',
+    assetScale: assetDetail?.node.scale || 0
+  })
+
+  if (!accountId) {
+    errors.message = ['Could not create account. Please try again!']
+    return json({ errors }, { status: 400 })
+  }
+
+  await createWallet({ ...result.data, accountId })
+
+  const session = await messageStorage.getSession(request.headers.get('cookie'))
+
+  return setMessageAndRedirect({
+    session,
+    message: {
+      content: 'Account created.',
+      type: 'success'
+    },
+    location: `/accounts/${accountId}`
+  })
+}
