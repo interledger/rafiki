d6abdebe6c3203fb954d965286a2fab7a323701c
Arpi <arpad.lengyel@breakpointit.eu>
2024-05-22 11:47:10 +0300

    add tailwind, packages, configs

---
+++ localenv/mock-account-servicing-entity/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./app/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        pearl: '#eee6e2',
+        offwhite: '#fbf7f4',
+        wafer: '#e3d7d0',
+        mercury: '#e2e2e2',
+        tealish: '#1e3250',
+        vermillion: '#c94343'
+      }
+    }
+  },
+  plugins: [require('@tailwindcss/forms')]
+}
