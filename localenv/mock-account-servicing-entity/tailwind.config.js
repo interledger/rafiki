c5810da33bdad62e3cff7aba71c4fe2164eb704f
Arpi <arpad.lengyel@breakpointit.eu>
2024-05-30 16:15:16 +0300

    new colors and backgrounds

--- localenv/mock-account-servicing-entity/tailwind.config.js
+++ localenv/mock-account-servicing-entity/tailwind.config.js
@@ -9,7 +9,13 @@ module.exports = {
         wafer: '#e3d7d0',
         mercury: '#e2e2e2',
         tealish: '#1e3250',
-        vermillion: '#c94343'
+        vermillion: '#c94343',
+        main_blue: '#3533A0',
+        secondary_blue: '#5553a5'
+      },
+      backgroundImage: {
+        wallet: "url('/wallet-background.png')",
+        bank: "url('/bank-background.png')"
       }
     }
   },
