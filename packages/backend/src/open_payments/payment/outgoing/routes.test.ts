92f3288c751c3582ed8b283250c43ff17312a07e
Blair Currey <12960453+BlairCurrey@users.noreply.github.com>
2024-06-05 21:43:39 -0400

    fix(backend): route test

--- packages/backend/src/open_payments/payment/outgoing/routes.test.ts
+++ packages/backend/src/open_payments/payment/outgoing/routes.test.ts
@@ -259,6 +259,16 @@ describe('Outgoing Payment Routes', (): void => {
               assetCode: walletAddress.asset.code,
               assetScale: walletAddress.asset.scale
             },
+            grantSpentDebitAmount: {
+              value: '0',
+              assetCode: walletAddress.asset.code,
+              assetScale: walletAddress.asset.scale
+            },
+            grantSpentReceiveAmount: {
+              value: '0',
+              assetCode: walletAddress.asset.code,
+              assetScale: walletAddress.asset.scale
+            },
             failed: false,
             createdAt: expect.any(String),
             updatedAt: expect.any(String)
