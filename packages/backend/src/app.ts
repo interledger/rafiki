c1a6b66d4f3e2b74a3cb4f0b4ca1142c61fdcbbe
Nathan Lie <lie4nathan@gmail.com>
2024-06-11 14:22:50 -0700

    feat(backend): secure admin api from replay attacks

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -373,7 +373,7 @@ export class App {
       }
     )
 
-    if (this.config.apiSecret) {
+    if (this.config.adminApiSecret) {
       koa.use(async (ctx, next: Koa.Next): Promise<void> => {
         if (!verifyApiSignature(ctx, this.config)) {
           ctx.throw(401, 'Unauthorized')
