92eac565cfe31da834e46980b8093ac23d069475
JoblersTune <sarah@interledger.org>
2024-07-01 12:51:24 +0200

    feat: added telemetry data * Added counts for the number of prepare packets a connector node recieves and the number of fulfill and reject packets it sends out.

--- packages/backend/src/payment-method/ilp/connector/core/middleware/telemetry.ts
+++ packages/backend/src/payment-method/ilp/connector/core/middleware/telemetry.ts
@@ -6,17 +6,44 @@ export function createTelemetryMiddleware(): ILPMiddleware {
     { request, services, accounts, response }: ILPContext,
     next: () => Promise<void>
   ): Promise<void> => {
+    if (services.telemetry && Number(request.prepare.amount)) {
+      services.telemetry
+        ?.getOrCreateMetric('packet_count_prepare', {
+          description: 'Count of incoming prepare packets'
+        })
+        .add(1, {
+          source: services.telemetry.getInstanceName()
+        })
+    }
+
     await next()
+
     if (
       services.telemetry &&
-      Number(request.prepare.amount) &&
-      response.fulfill
+      Number(request.prepare.amount)
     ) {
-      const { code, scale } = accounts.outgoing.asset
-      collectTelemetryAmount(services.telemetry, services.logger, {
-        amount: BigInt(request.prepare.amount),
-        asset: { code: code, scale: scale }
-      })
+      if (response.fulfill) {
+        const { code, scale } = accounts.outgoing.asset
+        collectTelemetryAmount(services.telemetry, services.logger, {
+          amount: BigInt(request.prepare.amount),
+          asset: { code: code, scale: scale }
+        })
+        services.telemetry
+        ?.getOrCreateMetric('packet_count_fulfill', {
+          description: 'Count of outgoing fulfill packets'
+        })
+        .add(1, {
+          source: services.telemetry.getInstanceName()
+        })
+      } else if (response.reject) {
+        services.telemetry
+        ?.getOrCreateMetric('packet_count_reject', {
+          description: 'Count of outgoing reject packets'
+        })
+        .add(1, {
+          source: services.telemetry.getInstanceName()
+        })
+      }
     }
   }
 }
