57b252667eebadb2d5d4280ba68b6778a01a65e6
golobitch <tadej.golobic@icloud.com>
2024-08-08 23:27:30 +0200

    fix(config): pool vs poll typo

--- packages/backend/src/config/app.ts
+++ packages/backend/src/config/app.ts
@@ -132,9 +132,9 @@ export const Config = {
 
   incomingPaymentWorkers: envInt('INCOMING_PAYMENT_WORKERS', 1),
   incomingPaymentWorkerIdle: envInt('INCOMING_PAYMENT_WORKER_IDLE', 200), // milliseconds
-  poolIncomingPaymentCreatedWebhook: envBool('POLL_INCOMING_PAYMENT_CREATED_WEBHOOK', false),
-  incomingPaymentCreatedPoolTimeout: envInt('INCOMING_PAYMENT_CREATED_POLL_TIMEOUT_MS', 10000), // milliseconds
-  incomingPaymentCreatedPoolFrequence: envInt('INCOMING_PAYMENT_CREATED_POLL_FREQUENCY_MS', 1000), // milliseconds
+  pollIncomingPaymentCreatedWebhook: envBool('POLL_INCOMING_PAYMENT_CREATED_WEBHOOK', false),
+  incomingPaymentCreatedPollTimeout: envInt('INCOMING_PAYMENT_CREATED_POLL_TIMEOUT_MS', 10000), // milliseconds
+  incomingPaymentCreatedPollFrequency: envInt('INCOMING_PAYMENT_CREATED_POLL_FREQUENCY_MS', 1000), // milliseconds
   
   webhookWorkers: envInt('WEBHOOK_WORKERS', 1),
   webhookWorkerIdle: envInt('WEBHOOK_WORKER_IDLE', 200), // milliseconds
