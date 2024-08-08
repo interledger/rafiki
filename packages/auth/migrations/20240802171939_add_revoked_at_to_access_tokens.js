8f3c147ba826b69c42b7a0c8c1def60f3d06f2e5
Nathan Lie <lie4nathan@gmail.com>
2024-08-08 07:22:19 -0700

    feat(auth): soft delete access tokens and grant accesses (#2837)

---
+++ packages/auth/migrations/20240802171939_add_revoked_at_to_access_tokens.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.alterTable('accessTokens', function (table) {
+    table.timestamp('revokedAt')
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.alterTable('accessTokens', function (table) {
+    table.dropColumn('revokedAt')
+  })
+}
