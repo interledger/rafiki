5e8156950a4806565dbb9b54d67da3cdb9fdf4b3
Sabine Schaller <sabine@interledger.org>
2024-08-15 11:23:03 +0200

    fix: build

--- packages/backend/src/tests/walletAddressKey.ts
+++ packages/backend/src/tests/walletAddressKey.ts
@@ -4,6 +4,7 @@ import { WalletAddressKey } from '../open_payments/wallet_address/key/model'
 import { CreateOptions } from '../open_payments/wallet_address/key/service'
 import { v4 as uuidv4 } from 'uuid'
 import { generateJwk, generateKey } from '@interledger/http-signature-utils'
+import { isWalletAddressKeyError } from '../open_payments/wallet_address/key/errors'
 export async function createWalletAddressKey(
   deps: IocContract<AppServices>,
   walletAddressId: string
@@ -18,5 +19,9 @@ export async function createWalletAddressKey(
     })
   }
 
-  return walletAddressKeyService.create(options)
+  const keyOrError = await walletAddressKeyService.create(options)
+  if (isWalletAddressKeyError(keyOrError)) {
+    throw keyOrError
+  }
+  return keyOrError
 }
