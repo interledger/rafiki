6b43cef8ff9fce343061c99305d80fa28f974ae3
Sabine Schaller <sabine@interledger.org>
2024-08-15 11:13:56 +0200

    chore: formatting

--- packages/backend/migrations/20240731084359_add_unique_constraint_to_kid.js
+++ packages/backend/migrations/20240731084359_add_unique_constraint_to_kid.js
@@ -2,7 +2,7 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
+exports.up = function (knex) {
   // delete any existing duplicates per wallet address
   knex.raw(`
   DELETE FROM walletAddressKeys
@@ -11,19 +11,19 @@ exports.up = function(knex) {
       FROM walletAddressKeys
       GROUP BY walletAddressId, kid, x
     );
-  `);
+  `)
 
   return knex.schema.alterTable('walletAddressKeys', (table) => {
-    table.unique(['walletAddressId', 'kid', 'x']);
-  });
-};
+    table.unique(['walletAddressId', 'kid', 'x'])
+  })
+}
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
+exports.down = function (knex) {
   return knex.schema.alterTable('walletAddressKeys', (table) => {
-    table.dropUnique(['walletAddressId', 'kid', 'x']);
-  });
-};
+    table.dropUnique(['walletAddressId', 'kid', 'x'])
+  })
+}
