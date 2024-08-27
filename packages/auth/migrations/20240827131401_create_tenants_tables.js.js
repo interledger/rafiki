bb8d2a4bb7d9308a159426018fdcdd77f802738a
bsanduc <bogdan.sandu@breakpointit.eu>
2024-08-27 16:56:56 +0300

    Update migration files

--- packages/auth/migrations/20240827131401_create_tenants_tables.js.js
+++ packages/auth/migrations/20240827131401_create_tenants_tables.js.js
@@ -5,7 +5,8 @@
 exports.up = function (knex) {
   return knex.schema.createTable('tenants', function (table) {
     table.uuid('id').primary()
-    table.string('kratosUrl').notNullable()
+    table.string('idpConsentEndpoint').notNullable()
+    table.string('idpSecret').notNullable()
     table.timestamp('createdAt').defaultTo(knex.fn.now())
     table.timestamp('updatedAt').defaultTo(knex.fn.now())
     table.timestamp('deletedAt').nullable().defaultTo(null)
