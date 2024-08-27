b311b5ca3a187f9e117dd1be28a7d719299aeee5
golobitch <tadej.golobic@icloud.com>
2024-08-27 17:08:49 +0300

    feat(tenant): basic tenant admin api schema and service

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -757,6 +757,9 @@ export const start = async (
   }
 
   await app.createOperatorIdentity()
+
+  await app.createOperatorIdentity()
+  logger.info('Operator identity created on Kratos')
 }
 
 // If this script is run directly, start the server
