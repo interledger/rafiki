5dfa6c4f6d2efca764b8d0575410dbcd9abe0e15
bsanduc <bogdan.sandu@breakpointit.eu>
2024-08-27 16:27:37 +0300

    Add migration files

---
+++ packages/auth/migrations/20240827131417_update_grants_with_tenantId.js.js
@@ -0,0 +1,25 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.table('grants', function (table) {
+    table.uuid('tenantId').notNullable()
+    table
+      .foreign('tenantId')
+      .references('id')
+      .inTable('tenants')
+      .onDelete('CASCADE')
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.table('grants', function (table) {
+    table.dropForeign(['tenantId'])
+    table.dropColumn('tenantId')
+  })
+}
