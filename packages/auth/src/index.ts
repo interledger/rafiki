7090b0089ef9abe632914dee2d472242fc69e02f
golobitch <tadej.golobic@icloud.com>
2024-08-27 18:02:23 +0300

    feat(auth): create basic tenant service and model plus graphql schema

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -93,6 +93,15 @@ export function initIocContainer(
     })
   })
 
+  container.singleton('tenantService', async (deps: IocContract<AppServices>) => {
+    const [logger, knex] = await Promise.all([
+      deps.use('logger'),
+      deps.use('knex')
+    ])
+
+    return createTenantService({ logger, knex })
+  })
+
   container.singleton(
     'tenantService',
     async (deps: IocContract<AppServices>) => {
