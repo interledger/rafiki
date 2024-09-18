27a64b23784502dd9ed49cb989b52d65d5e55347
Blair Currey <12960453+BlairCurrey@users.noreply.github.com>
2024-09-18 10:53:22 -0400

    chore(backend): format

--- packages/backend/migrations/20240916181643_require_estimated_exchange_rate.js
+++ packages/backend/migrations/20240916181643_require_estimated_exchange_rate.js
@@ -4,30 +4,30 @@
  */
 
 exports.up = function (knex) {
-  return knex("quotes")
-    .whereNull("estimatedExchangeRate")
+  return knex('quotes')
+    .whereNull('estimatedExchangeRate')
     .update({
       // TODO: vet this more... looks like the low* fields were (inadvertently?)
       // made nullable when updating from bigint to decimal. If they are null
       // anywhere then this wont work.
-      estimatedExchangeRate: knex.raw("?? / ??", [
-        "lowEstimatedExchangeRateNumerator",
-        "lowEstimatedExchangeRateDenominator",
-      ]),
+      estimatedExchangeRate: knex.raw('?? / ??', [
+        'lowEstimatedExchangeRateNumerator',
+        'lowEstimatedExchangeRateDenominator'
+      ])
     })
     .then(() => {
-      return knex.schema.table("quotes", (table) => {
-        table.decimal("estimatedExchangeRate", 20, 10).notNullable().alter();
-      });
-    });
-};
+      return knex.schema.table('quotes', (table) => {
+        table.decimal('estimatedExchangeRate', 20, 10).notNullable().alter()
+      })
+    })
+}
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
 exports.down = function (knex) {
-  return knex.schema.table("quotes", (table) => {
-    table.decimal("estimatedExchangeRate", 20, 10).nullable().alter();
-  });
-};
+  return knex.schema.table('quotes', (table) => {
+    table.decimal('estimatedExchangeRate', 20, 10).nullable().alter()
+  })
+}
