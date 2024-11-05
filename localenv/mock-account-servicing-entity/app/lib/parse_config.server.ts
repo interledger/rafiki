d30deeb2b683d0cde91a316eabcfda5ba33da810
oana-lolea <oana.lolea@breakpointit.eu>
2024-11-05 11:53:27 +0200

    chore: update frontend environment variables (#3030)
    * Updated frontend environment variables and their corresponding doocumentation
    
    * fixed import issue of kratos admin url
    
    * Cookie 'secure' attr. set to true by default, removed unnecessary docker environment variables

--- localenv/mock-account-servicing-entity/app/lib/parse_config.server.ts
+++ localenv/mock-account-servicing-entity/app/lib/parse_config.server.ts
@@ -3,8 +3,16 @@ import { readFileSync } from 'fs'
 import { loadOrGenerateKey } from '@interledger/http-signature-utils'
 import type { Config } from 'mock-account-service-lib'
 
+if (!process.env.OPEN_PAYMENTS_URL) {
+  throw new Error('Environment variable OPEN_PAYMENTS_URL is required')
+}
+
+if (!process.env.GRAPHQL_URL) {
+  throw new Error('Environment variable GRAPHQL_URL is required')
+}
+
 if (!process.env.IDP_SECRET) {
-  throw new Error('environment variable IDP_SECRET is required')
+  throw new Error('Environment variable IDP_SECRET is required')
 }
 
 export const CONFIG: Config = {
@@ -14,9 +22,9 @@ export const CONFIG: Config = {
     ).toString('utf8')
   ),
   key: loadOrGenerateKey(process.env.KEY_FILE),
-  publicHost: process.env.OPEN_PAYMENTS_URL ?? '',
+  publicHost: process.env.OPEN_PAYMENTS_URL,
   testnetAutoPeerUrl: process.env.TESTNET_AUTOPEER_URL ?? '',
   authServerDomain: process.env.AUTH_SERVER_DOMAIN || 'http://localhost:3006',
-  graphqlUrl: process.env.GRAPHQL_URL ?? '',
+  graphqlUrl: process.env.GRAPHQL_URL,
   idpSecret: process.env.IDP_SECRET
 }
