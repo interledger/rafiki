e6cad1e808cbcbe28440ace51c8501534d8f7e82
Blair Currey <12960453+BlairCurrey@users.noreply.github.com>
2024-11-14 11:27:22 -0500

    chore(backend): format

--- packages/backend/src/payment-method/ilp/connector/core/test/middleware/balance-middleware.test.ts
+++ packages/backend/src/payment-method/ilp/connector/core/test/middleware/balance-middleware.test.ts
@@ -73,12 +73,10 @@ describe('Balance Middleware', function () {
       ctx.response.fulfill = fulfill
     })
     const destinationAmount = BigInt(200)
-    jest
-      .spyOn(rates, 'convert')
-      .mockImplementationOnce(async () => ({
-        amount: destinationAmount,
-        scaledExchangeRate: 1
-      }))
+    jest.spyOn(rates, 'convert').mockImplementationOnce(async () => ({
+      amount: destinationAmount,
+      scaledExchangeRate: 1
+    }))
 
     await expect(middleware(ctx, next)).resolves.toBeUndefined()
 
