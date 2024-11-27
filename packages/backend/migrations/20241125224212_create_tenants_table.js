2887c32f1d67532f284de3c0c51b95d2880b4cba
Nathan Lie <lie4nathan@gmail.com>
2024-11-27 10:20:34 -0800

    feat(backend): tenant service

--- packages/backend/migrations/20241125224212_create_tenants_table.js
+++ packages/backend/migrations/20241125224212_create_tenants_table.js
@@ -8,6 +8,9 @@ exports.up = function (knex) {
     table.string('email').notNullable()
     table.string('publicName')
     table.string('apiSecret')
+
+    table.timestamp('createdAt').defaultTo(knex.fn.now())
+    table.timestamp('updatedAt').defaultTo(knex.fn.now())
   })
 }
 
