1da190f3dec1e92fa561169b6bd2fae9728b8927
Max Kurapov <max@interledger.org>
2024-12-09 13:45:20 +0100

    feat(auth,backend): update migrations with error messages

--- packages/backend/migrations/20241205153035_seed_operator_tenant.js
+++ packages/backend/migrations/20241205153035_seed_operator_tenant.js
@@ -2,10 +2,20 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
+
+const OPERATOR_TENANT_ID = process.env['OPERATOR_TENANT_ID']
+const OPERATOR_TENANT_SECRET = process.env['OPERATOR_TENANT_SECRET']
+
 exports.up = function (knex) {
+  if (!OPERATOR_TENANT_ID || !OPERATOR_TENANT_SECRET) {
+    throw new Error(
+      'Could not seed operator tenant. Please configure OPERATOR_TENANT_ID and OPERATOR_TENANT_SECRET environment variables'
+    )
+  }
+
   return knex.raw(`
         INSERT INTO "tenants" ("id", "apiSecret") 
-        VALUES ('${process.env['OPERATOR_TENANT_ID']}', '${process.env['OPERATOR_TENANT_SECRET']}')
+        VALUES ('${OPERATOR_TENANT_ID}', '${OPERATOR_TENANT_SECRET}')
     `)
 }
 
