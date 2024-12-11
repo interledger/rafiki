10bc368bbc2a2137538d388ca35464b401afd0ef
Max Kurapov <max@interledger.org>
2024-12-11 10:54:52 +0100

    feat(auth, backend): seed operator tenant (#3156)
    * feat(auth): migration to seed operator tenant
    
    * feat(backend): migration to seed operator tenant
    
    * chore(localenv): add env vars for operator tenant
    
    * test(backend): set operator env variables in jest config
    
    * test(auth): set operator env variables in jest config
    
    * test(auth, backend): load env vars into jest environment script
    
    * feat(auth,backend): update migrations with error messages
    
    * test(integration): adding operator tenant vars
    
    * chore(backend, localenv): replace OPERATOR_TENANT_SECRET with existing API_SECRET

--- packages/backend/jest.config.js
+++ packages/backend/jest.config.js
@@ -4,26 +4,12 @@ const baseConfig = require('../../jest.config.base.js')
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const packageName = require('./package.json').name
 
-process.env.LOG_LEVEL = 'silent'
-process.env.INSTANCE_NAME = 'Rafiki'
-process.env.KEY_ID = 'myKey'
-process.env.OPEN_PAYMENTS_URL = 'http://127.0.0.1:3000'
-process.env.ILP_CONNECTOR_URL = 'http://127.0.0.1:3002'
-process.env.ILP_ADDRESS = 'test.rafiki'
-process.env.AUTH_SERVER_GRANT_URL = 'http://127.0.0.1:3006'
-process.env.AUTH_SERVER_INTROSPECTION_URL = 'http://127.0.0.1:3007/'
-process.env.AUTH_ADMIN_API_URL = 'http://127.0.0.1:3003/graphql'
-process.env.AUTH_ADMIN_API_SECRET = 'test-secret'
-process.env.WEBHOOK_URL = 'http://127.0.0.1:4001/webhook'
-process.env.STREAM_SECRET = '2/PxuRFV9PAp0yJlnAifJ+1OxujjjI16lN+DBnLNRLA='
-process.env.USE_TIGERBEETLE = false
-process.env.ENABLE_TELEMETRY = false
-
 module.exports = {
   ...baseConfig,
   clearMocks: true,
   testTimeout: 30000,
   roots: [`<rootDir>/packages/${packageName}`],
+  setupFiles: [`<rootDir>/packages/${packageName}/jest.env.js`],
   globalSetup: `<rootDir>/packages/${packageName}/jest.setup.ts`,
   globalTeardown: `<rootDir>/packages/${packageName}/jest.teardown.js`,
   testRegex: `(packages/${packageName}/.*/__tests__/.*|\\.(test|spec))\\.tsx?$`,
