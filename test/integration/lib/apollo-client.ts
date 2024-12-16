5db072edee7d6ed5ac00546f26ce85684a76b2b1
Max Kurapov <max@interledger.org>
2024-12-16 12:45:11 +0100

    feat(integration): sign Admin API requests during integration tests (#3177)
    * fix(backend): await signature verification
    
    * test(integration): add signatures to apollo client requests
    
    * test(backend): sign GraphQL requests in test environment
    
    * Revert "test(backend): sign GraphQL requests in test environment"
    
    This reverts commit 0a128d1b0ebad8281a7e6401da3b25c628c5a033.
    
    * chore(backend): remove sig verification in test files

---
+++ test/integration/lib/apollo-client.ts
@@ -0,0 +1,66 @@
+import type { NormalizedCacheObject } from '@apollo/client'
+import {
+  ApolloClient,
+  ApolloLink,
+  createHttpLink,
+  InMemoryCache
+} from '@apollo/client'
+import { createHmac } from 'crypto'
+import { print } from 'graphql/language/printer'
+import { canonicalize } from 'json-canonicalize'
+import { setContext } from '@apollo/client/link/context'
+
+interface CreateApolloClientArgs {
+  graphqlUrl: string
+  signatureSecret: string
+  signatureVersion: string
+}
+
+function createAuthLink(args: CreateApolloClientArgs) {
+  return setContext((request, { headers }) => {
+    const timestamp = Math.round(new Date().getTime() / 1000)
+    const version = args.signatureVersion
+
+    const { query, variables, operationName } = request
+    const formattedRequest = {
+      variables,
+      operationName,
+      query: print(query)
+    }
+
+    const payload = `${timestamp}.${canonicalize(formattedRequest)}`
+    const hmac = createHmac('sha256', args.signatureSecret)
+    hmac.update(payload)
+    const digest = hmac.digest('hex')
+    return {
+      headers: {
+        ...headers,
+        signature: `t=${timestamp}, v${version}=${digest}`
+      }
+    }
+  })
+}
+
+export function createApolloClient(
+  args: CreateApolloClientArgs
+): ApolloClient<NormalizedCacheObject> {
+  const httpLink = createHttpLink({
+    uri: args.graphqlUrl
+  })
+
+  return new ApolloClient({
+    link: ApolloLink.from([createAuthLink(args), httpLink]),
+    cache: new InMemoryCache(),
+    defaultOptions: {
+      query: {
+        fetchPolicy: 'no-cache'
+      },
+      mutate: {
+        fetchPolicy: 'no-cache'
+      },
+      watchQuery: {
+        fetchPolicy: 'no-cache'
+      }
+    }
+  })
+}
