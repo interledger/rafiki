c16aed4fa4270e7cdad9c255f6dd4cb6bcbefd3e
Nathan Lie <lie4nathan@gmail.com>
2024-12-18 13:35:17 -0800

    fix: tests

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -426,6 +426,7 @@ export function initIocContainer(
   })
   container.singleton('remoteIncomingPaymentService', async (deps) => {
     return await createRemoteIncomingPaymentService({
+      config: await deps.use('config'),
       logger: await deps.use('logger'),
       knex: await deps.use('knex'),
       grantService: await deps.use('grantService'),
