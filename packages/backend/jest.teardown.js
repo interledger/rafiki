20dc85b951447024a870a663396c6ca8d9499ab2
Nathan Lie <lie4nathan@gmail.com>
2025-01-22 14:12:55 -0800

    fix: extra testing db for tenants

--- packages/backend/jest.teardown.js
+++ packages/backend/jest.teardown.js
@@ -3,7 +3,12 @@ module.exports = async () => {
     { directory: __dirname + '/migrations' },
     true
   )
+  await global.__BACKEND_TENANT_KNEX__.migrate.rollback(
+    { directory: __dirname + '/migrations' },
+    true
+  )
   await global.__BACKEND_KNEX__.destroy()
+  await global.__BACKEND_TENANT_KNEX__.destroy()
   if (global.__BACKEND_POSTGRES__) {
     await global.__BACKEND_POSTGRES__.stop()
   }
