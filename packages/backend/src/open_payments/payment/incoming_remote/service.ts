a4d165e8d2d9ba36c698272b453cbdc5336cdf33
oana-lolea <oana.lolea@breakpointit.eu>
2025-02-17 12:51:02 +0200

    feat(backend): tenanted incoming payments (#3271)
    * Draft version of tenanted incoming payments
    
    * Finished fixing unit and integration tests
    
    * PR comments fixes
    
    * Created helper function that adds tenantId to incoming payment id
    
    * Removed tenantId from url in bruno requests, added tenantId in url of the incoming payment, fixed tests accordingly
    
    * Fixed formatting
    
    * Replaced get with findOne when querying using id and tenantId.

--- packages/backend/src/open_payments/payment/incoming_remote/service.ts
+++ packages/backend/src/open_payments/payment/incoming_remote/service.ts
@@ -14,7 +14,6 @@ import { BaseService } from '../../../shared/baseService'
 import { Amount, serializeAmount } from '../../amount'
 import { RemoteIncomingPaymentError } from './errors'
 import { isGrantError } from '../../grant/errors'
-import { urlWithoutTenantId } from '../../../shared/utils'
 
 interface CreateRemoteIncomingPaymentArgs {
   walletAddressUrl: string
@@ -126,7 +125,7 @@ async function createIncomingPayment(
   try {
     return await deps.openPaymentsClient.incomingPayment.create(
       {
-        url: urlWithoutTenantId(resourceServerUrl),
+        url: resourceServerUrl,
         accessToken: grant.accessToken
       },
       {
