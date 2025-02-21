1245e034136d3beae53fa1cb38dbe8ad991e63ec
Nathan Lie <lie4nathan@gmail.com>
2025-02-21 10:22:11 -0800

    feat(backend): tenanted webhooks

--- packages/backend/src/tenants/settings/service.test.ts
+++ packages/backend/src/tenants/settings/service.test.ts
@@ -275,7 +275,7 @@ describe('TenantSetting Service', (): void => {
       )
       expect(dbTenantSetting?.deletedAt).toBeDefined()
 
-      const originalDeletedAt = dbTenantSetting?.deletedAt;
+      const originalDeletedAt = dbTenantSetting?.deletedAt
       await tenantSettingService.delete({
         tenantId: tenantSetting[0].tenantId,
         key: createOptions.setting[0].key
@@ -286,7 +286,9 @@ describe('TenantSetting Service', (): void => {
       )
       expect(dbTenantSetting?.deletedAt).toBeDefined()
 
-      expect(originalDeletedAt?.getTime()).toEqual(dbTenantSetting?.deletedAt?.getTime())
+      expect(originalDeletedAt?.getTime()).toEqual(
+        dbTenantSetting?.deletedAt?.getTime()
+      )
     })
 
     test('can delete all tenant settings', async (): Promise<void> => {
