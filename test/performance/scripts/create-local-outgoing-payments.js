cddcc34803260413ca712c1c9bd782db80390e31
Blair Currey <12960453+BlairCurrey@users.noreply.github.com>
2025-02-23 22:45:45 -0500

    feat: add local payment test script

---
+++ test/performance/scripts/create-local-outgoing-payments.js
@@ -0,0 +1,167 @@
+import http from 'k6/http'
+import { fail } from 'k6'
+import { createHMAC } from 'k6/crypto'
+import { uuidv4 } from 'https://jslib.k6.io/k6-utils/1.4.0/index.js'
+import { canonicalize } from '../dist/json-canonicalize.bundle.js'
+
+export const options = {
+  vus: 50,
+  duration: '120s'
+  // vus: 1,
+  // iterations: 1
+}
+
+// const CLOUD_NINE_GQL_ENDPOINT = 'http://cloud-nine-wallet-backend:3001/graphql'
+// const CLOUD_NINE_WALLET_ADDRESS =
+//   'https://cloud-nine-wallet-backend/accounts/gfranklin'
+// const HAPPY_LIFE_BANK_WALLET_ADDRESS =
+//   'https://happy-life-bank-backend/accounts/pfry'
+
+const GQL_ENDPOINT = 'http://cloud-nine-wallet-backend:3001/graphql'
+const SENDER_WALLET_ADDRESS =
+  'https://cloud-nine-wallet-backend/accounts/gfranklin'
+const RECEIVER_WALLET_ADDRESS =
+  'https://cloud-nine-wallet-backend/accounts/bhamchest'
+const SIGNATURE_SECRET = 'iyIgCprjb9uL8wFckR+pLEkJWMB7FJhgkvqhTQR/964='
+const SIGNATURE_VERSION = '1'
+
+function generateSignedHeaders(requestPayload) {
+  const timestamp = Date.now()
+  const payload = `${timestamp}.${canonicalize(requestPayload)}`
+  const hmac = createHMAC('sha256', SIGNATURE_SECRET)
+  hmac.update(payload)
+  const digest = hmac.digest('hex')
+
+  return {
+    'Content-Type': 'application/json',
+    signature: `t=${timestamp}, v${SIGNATURE_VERSION}=${digest}, n=${uuidv4()}`
+  }
+}
+
+function request(query) {
+  const headers = generateSignedHeaders(query)
+  const response = http.post(GQL_ENDPOINT, JSON.stringify(query), {
+    headers
+  })
+
+  if (response.status !== 200) {
+    fail(`GraphQL Request failed`)
+  }
+  return JSON.parse(response.body).data
+}
+
+export function setup() {
+  const query = {
+    query: `
+      query GetWalletAddresses {
+        walletAddresses {
+          edges {
+            node {
+              id
+              url
+            }
+          }
+        }
+      }
+    `
+  }
+
+  const data = request(query)
+  const c9WalletAddresses = data.walletAddresses.edges
+  const c9WalletAddress = c9WalletAddresses.find(
+    (edge) => edge.node.url === SENDER_WALLET_ADDRESS
+  ).node
+  if (!c9WalletAddress) {
+    fail(`could not find wallet address: ${SENDER_WALLET_ADDRESS}`)
+  }
+
+  return { data: { c9WalletAddress } }
+}
+
+// The function that defines VU logic.
+//
+// See https://grafana.com/docs/k6/latest/examples/get-started-with-k6/ to learn more
+// about authoring k6 scripts.
+//
+export default function (data) {
+  const {
+    data: { c9WalletAddress }
+  } = data
+
+  const createReceiverPayload = {
+    query: `
+      mutation CreateReceiver($input: CreateReceiverInput!) {
+        createReceiver(input: $input) {
+          receiver {
+            id
+          }
+        }
+      }
+    `,
+    variables: {
+      input: {
+        expiresAt: null,
+        metadata: {
+          description: 'Hello my friend',
+          externalRef: null
+        },
+        incomingAmount: {
+          assetCode: 'USD',
+          assetScale: 2,
+          value: 1002
+        },
+        walletAddressUrl: RECEIVER_WALLET_ADDRESS
+      }
+    }
+  }
+
+  const createReceiverResponse = request(createReceiverPayload)
+  const receiver = createReceiverResponse.createReceiver.receiver
+
+  const createQuotePayload = {
+    query: `
+        mutation CreateQuote($input: CreateQuoteInput!) {
+          createQuote(input: $input) {
+            quote {
+              id
+            }
+          }
+        }
+      `,
+    variables: {
+      input: {
+        walletAddressId: c9WalletAddress.id,
+        receiveAmount: null,
+        receiver: receiver.id,
+        debitAmount: {
+          assetCode: 'USD',
+          assetScale: 2,
+          value: 500
+        }
+      }
+    }
+  }
+
+  const createQuoteResponse = request(createQuotePayload)
+  const quote = createQuoteResponse.createQuote.quote
+
+  const createOutgoingPaymentPayload = {
+    query: `
+      mutation CreateOutgoingPayment($input: CreateOutgoingPaymentInput!) {
+        createOutgoingPayment(input: $input) {
+          payment {
+            id
+          }
+        }
+      }
+    `,
+    variables: {
+      input: {
+        walletAddressId: c9WalletAddress.id,
+        quoteId: quote.id
+      }
+    }
+  }
+
+  request(createOutgoingPaymentPayload)
+}
