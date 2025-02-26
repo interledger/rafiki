a5b280dbe41fa657e40a3df4603bb255601024ac
Nathan Lie <lie4nathan@gmail.com>
2025-02-25 16:10:58 -0800

    feat: add tenant id to tests; temporary operator tenant id in peer webhook events

--- packages/backend/src/webhook/service.ts
+++ packages/backend/src/webhook/service.ts
@@ -201,7 +201,6 @@ async function sendWebhookEvent(
       )
     }
 
-<<<<<<< HEAD
     await axios.post(settings?.webhookUrl ?? deps.config.webhookUrl, body, {
       timeout: Number(settings?.webhookTimeout)
         ? Number(settings?.webhookTimeout)
@@ -209,19 +208,6 @@ async function sendWebhookEvent(
       headers: requestHeaders,
       validateStatus: (status) => status === 200
     })
-=======
-    await axios.post(
-      settings?.webhookUrl?.value ?? deps.config.webhookUrl,
-      body,
-      {
-        timeout: settings?.webhookTimeout?.value
-          ? Number(settings?.webhookTimeout?.value)
-          : deps.config.webhookTimeout,
-        headers: requestHeaders,
-        validateStatus: (status) => status === 200
-      }
-    )
->>>>>>> fdb4e2df1 (feat(backend): tenanted webhooks)
 
     await event.$query(deps.knex).patch({
       attempts: event.attempts + 1,
