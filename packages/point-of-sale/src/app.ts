b66c9448ba56a2f1dd0c71c6e66c9c3a517f035b
oana-lolea <oana.lolea@breakpointit.eu>
2025-07-08 12:25:33 +0300

    feat: initialize POS service (#3509)
    * feat(wip): pos service
    
    * Completed POS service init
    
    * Added docker files and missing scripts
    
    * Removed editor code after handling conflicts
    
    * Updated docker-compose for cloud 9 localenv and removed unnecessary telemetry command from dockerfile
    
    * Updated docker compose files for c9 and hlb
    
    * Updated ports and env var names from config
    
    * fix: install ts-node-dev, add main script to index.ts
    
    ---------
    
    Co-authored-by: Nathan Lie <lie4nathan@gmail.com>

---
+++ packages/point-of-sale/src/app.ts
@@ -0,0 +1,113 @@
+import { Knex } from 'knex'
+import { Logger } from 'pino'
+import { IAppConfig } from './config/app'
+import { Server } from 'http'
+import { IocContract } from '@adonisjs/fold'
+import Koa, { DefaultState } from 'koa'
+import Router from '@koa/router'
+import bodyParser from 'koa-bodyparser'
+import cors from '@koa/cors'
+
+export interface AppServices {
+  logger: Promise<Logger>
+  knex: Promise<Knex>
+  config: Promise<IAppConfig>
+}
+
+export type AppContainer = IocContract<AppServices>
+
+export interface AppContextData {
+  logger: Logger
+  container: AppContainer
+  // Set by @koa/router.
+  params: { [key: string]: string }
+}
+
+export type AppContext = Koa.ParameterizedContext<DefaultState, AppContextData>
+
+export class App {
+  private posServer!: Server
+  public isShuttingDown = false
+  private logger!: Logger
+  private config!: IAppConfig
+
+  public constructor(private container: IocContract<AppServices>) {}
+
+  public async boot(): Promise<void> {
+    this.config = await this.container.use('config')
+    this.logger = await this.container.use('logger')
+  }
+
+  public async startPosServer(port: number): Promise<void> {
+    const koa = await this.createKoaServer()
+
+    const router = new Router<DefaultState, AppContext>()
+    router.use(bodyParser())
+    router.get('/healthz', (ctx: AppContext): void => {
+      ctx.status = 200
+    })
+
+    koa.use(cors())
+    koa.use(router.routes())
+
+    this.posServer = koa.listen(port)
+  }
+
+  public async shutdown(): Promise<void> {
+    this.isShuttingDown = true
+
+    if (this.posServer) {
+      await this.stopServer(this.posServer)
+    }
+  }
+
+  private async stopServer(server: Server): Promise<void> {
+    return new Promise((resolve, reject) => {
+      server.close((err) => {
+        if (err) {
+          reject(err)
+        }
+
+        resolve()
+      })
+    })
+  }
+
+  public getPort() {
+    const address = this.posServer?.address()
+    if (address && !(typeof address == 'string')) {
+      return address.port
+    }
+    return 0
+  }
+
+  private async createKoaServer(): Promise<Koa<Koa.DefaultState, AppContext>> {
+    const koa = new Koa<DefaultState, AppContext>({
+      proxy: this.config.trustProxy
+    })
+
+    koa.context.container = this.container
+    koa.context.logger = await this.container.use('logger')
+
+    koa.use(
+      async (
+        ctx: {
+          status: number
+          set: (arg0: string, arg1: string) => void
+          body: string
+        },
+        next: () => void | PromiseLike<void>
+      ): Promise<void> => {
+        if (this.isShuttingDown) {
+          ctx.status = 503
+          ctx.set('Connection', 'close')
+          ctx.body = 'Server is in the process of restarting'
+        } else {
+          return next()
+        }
+      }
+    )
+
+    return koa
+  }
+}
---
+++ packages/point-of-sale/src/config/app.ts
@@ -0,0 +1,38 @@
+import dotenv from 'dotenv'
+
+function envString(name: string, defaultValue?: string): string {
+  const envValue = process.env[name]
+
+  if (envValue) return envValue
+  if (defaultValue) return defaultValue
+
+  throw new Error(`Environment variable ${name} must be set.`)
+}
+
+function envInt(name: string, value: number): number {
+  const envValue = process.env[name]
+  return envValue == null ? value : parseInt(envValue)
+}
+
+function envBool(name: string, value: boolean): boolean {
+  const envValue = process.env[name]
+  return envValue == null ? value : envValue === 'true'
+}
+
+export type IAppConfig = typeof Config
+
+dotenv.config({
+  path: process.env.ENV_FILE || '.env'
+})
+
+export const Config = {
+  logLevel: envString('LOG_LEVEL', 'info'),
+  databaseUrl: envString(
+    'DATABASE_URL',
+    'postgresql://postgres:password@localhost:5432/development'
+  ),
+  env: envString('NODE_ENV', 'development'),
+  port: envInt('PORT', 3008),
+  trustProxy: envBool('TRUST_PROXY', false),
+  enableManualMigrations: envBool('ENABLE_MANUAl_MIGRATIONS', false)
+}
