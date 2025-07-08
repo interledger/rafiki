93638a73040ed0360c1644c376fd8a02732b099d
Arpi Lengyel <arpad.lengyel@breakpointit.eu>
2025-07-08 11:13:21 +0300

    migration & model for card payments

---
+++ packages/card-service/src/card-payments/model.ts
@@ -0,0 +1,20 @@
+import { BaseModel } from '../shared/baseModel'
+
+export class CardPayments extends BaseModel {
+  public static get tableName(): string {
+    return 'card_payments'
+  }
+
+  public requestId!: string
+  public requestedAt!: Date | null
+  public finalizedAt!: Date | null
+  public cardWalletAddress!: string
+  public incomingPaymentUrl?: string
+  public statusCode?: number
+  public outgoingPaymentId?: string
+  public terminalId!: string
+
+
+
+
+}
