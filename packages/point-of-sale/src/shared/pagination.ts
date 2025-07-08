432eae313d7eeda2f80aa729d8e1559a7be67628
Arpi Lengyel <arpad.lengyel@breakpointit.eu>
2025-07-08 12:59:36 +0300

    shared

---
+++ packages/point-of-sale/src/shared/pagination.ts
@@ -0,0 +1,68 @@
+import { PaginationArgs } from '@interledger/open-payments'
+
+import { BaseModel, PageInfo, Pagination, SortOrder } from './baseModel'
+
+export function parsePaginationQueryParameters({
+  first,
+  last,
+  cursor
+}: PaginationArgs): Pagination {
+  return {
+    first,
+    last,
+    before: last ? cursor : undefined,
+    after: cursor && !last ? cursor : undefined
+  }
+}
+
+type GetPageInfoArgs<T extends BaseModel> = {
+  getPage: (pagination: Pagination, sortOrder?: SortOrder) => Promise<T[]>
+  page: T[]
+  sortOrder?: SortOrder
+}
+
+export async function getPageInfo<T extends BaseModel>({
+  getPage,
+  page,
+  sortOrder
+}: GetPageInfoArgs<T>): Promise<PageInfo> {
+  if (page.length == 0)
+    return {
+      hasPreviousPage: false,
+      hasNextPage: false
+    }
+  const firstId = page[0].id
+  const lastId = page[page.length - 1].id
+
+  let hasNextPage, hasPreviousPage
+
+  try {
+    hasNextPage = await getPage(
+      {
+        after: lastId,
+        first: 1
+      },
+      sortOrder
+    )
+  } catch (e) {
+    hasNextPage = []
+  }
+  try {
+    hasPreviousPage = await getPage(
+      {
+        before: firstId,
+        last: 1
+      },
+      sortOrder
+    )
+  } catch (e) {
+    hasPreviousPage = []
+  }
+
+  return {
+    endCursor: lastId,
+    hasNextPage: hasNextPage.length == 1,
+    hasPreviousPage: hasPreviousPage.length == 1,
+    startCursor: firstId
+  }
+}
