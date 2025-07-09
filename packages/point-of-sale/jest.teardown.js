bbf2fa28c62ef5e503560f1711b12c520da298f4
Arpi Lengyel <arpad.lengyel@breakpointit.eu>
2025-07-09 15:32:04 +0300

    jest cleanup

--- packages/point-of-sale/jest.teardown.js
+++ packages/point-of-sale/jest.teardown.js
@@ -1,13 +1,10 @@
 module.exports = async () => {
-  await global.__BACKEND_KNEX__.migrate.rollback(
+  await global.__POS_KNEX__.migrate.rollback(
     { directory: __dirname + '/migrations' },
     true
   )
-  await global.__BACKEND_KNEX__.destroy()
-  if (global.__BACKEND_POSTGRES__) {
-    await global.__BACKEND_POSTGRES__.stop()
-  }
-  if (global.__BACKEND_REDIS__) {
-    await global.__BACKEND_REDIS__.stop()
+  await global.__POS_KNEX__.destroy()
+  if (global.__POS_POSTGRES__) {
+    await global.__POS_POSTGRES__.stop()
   }
 }
