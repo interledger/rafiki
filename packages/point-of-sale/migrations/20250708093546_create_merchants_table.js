d5e02686e24db29c8e81b77b61e14a76dd3c2f57
Arpi Lengyel <arpad.lengyel@breakpointit.eu>
2025-07-09 15:05:14 +0300

    fix type

--- packages/point-of-sale/migrations/20250708093546_create_merchants_table.js
+++ packages/point-of-sale/migrations/20250708093546_create_merchants_table.js
@@ -10,7 +10,7 @@ exports.up = function (knex) {
 
     table.timestamp('createdAt').defaultTo(knex.fn.now())
     table.timestamp('updatedAt').defaultTo(knex.fn.now())
-    table.timestamp('deletedAt')
+    table.timestamp('deletedAt').nullable()
   })
 }
 
