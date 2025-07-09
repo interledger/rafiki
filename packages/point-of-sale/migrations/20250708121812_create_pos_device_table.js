7905bb40d6e4ef90ba1389625b993a087ba4e18b
Arpi Lengyel <165793743+lengyel-arpad85@users.noreply.github.com>
2025-07-09 11:12:46 +0300

    feat(pos): pos card services table (#3526)
    * merchants table migrations and model

---
+++ packages/point-of-sale/migrations/20250708121812_create_pos_device_table.js
@@ -0,0 +1,36 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable('posDevices', function (table) {
+    table.uuid('id').notNullable().primary()
+    table
+      .uuid('merchantId')
+      .notNullable()
+      .references('merchants.id')
+      .onDelete('CASCADE')
+      .index()
+
+    table.uuid('walletAddressId').notNullable()
+
+    table.string('deviceName').notNullable()
+    table.string('publicKey')
+    table.string('keyId')
+    table.string('algorithm')
+
+    table.string('status').notNullable()
+
+    table.timestamp('createdAt').defaultTo(knex.fn.now())
+    table.timestamp('updatedAt').defaultTo(knex.fn.now())
+    table.timestamp('deletedAt')
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists('posDevices')
+}
