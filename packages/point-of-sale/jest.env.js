579032fc3cb25886eb98239dcef7b78947903271
Antoniu Neacsu <kantoniu98@gmail.com>
2025-07-10 17:00:12 +0300

    prettier fix

--- packages/point-of-sale/jest.env.js
+++ packages/point-of-sale/jest.env.js
@@ -1,4 +1,4 @@
-process.env.TENANT_ID="tenant_id"
-process.env.TENANT_SECRET="tenant_secret"
-process.env.TENANT_SIGNATURE_VERSION="tenant_signature_version"
-process.env.GRAPHQL_URL="http://127.0.0.1:3003/graphq"
+process.env.TENANT_ID = 'tenant_id'
+process.env.TENANT_SECRET = 'tenant_secret'
+process.env.TENANT_SIGNATURE_VERSION = 'tenant_signature_version'
+process.env.GRAPHQL_URL = 'http://127.0.0.1:3003/graphq'
