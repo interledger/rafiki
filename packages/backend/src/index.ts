d74513118fd5f18b36ee9daf7372ea4a63fef1df
MiguelLescasJorgeSebastian <127277214+MiguelLescasJorgeSebastian@users.noreply.github.com>
2025-07-10 15:54:28 +0300

    fix: (rafiki backend) Augment Wallet Address response with cardService field (#3552)
    * feat: add cardServiceUrl configuration and update related routes
    
    * feat: rename CARD_SERVICE_HOST to CARD_SERVICE_URL for consistency
    
    * fix format
    
    ---------
    
    Co-authored-by: HRadry <adriana@lacamara.dev>

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -676,7 +676,7 @@ export function initIocContainer(
     return createCardService({
       axios: await deps.use('axios'),
       logger: await deps.use('logger'),
-      cardServiceHost: config.cardServiceHost
+      cardServiceUrl: config.cardServiceUrl
     })
   })
 
