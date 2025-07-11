e89edaa0c4e03a1616bc9affd1e9afc6a17276fc
oana-lolea <oana.lolea@breakpointit.eu>
2025-07-11 07:09:37 +0300

    feat(point-of-sale): added route for registering a POS device (#3555)
    * Route for registering a POS device
    
    * Fixed issue addressed in comments, tried to fix the port issue of jest
    
    * Changed test container port to 0, format fix

--- packages/point-of-sale/src/index.ts
+++ packages/point-of-sale/src/index.ts
@@ -7,6 +7,7 @@ import createLogger from 'pino'
 import { createMerchantService } from './merchant/service'
 import { createPosDeviceService } from './merchant/devices/service'
 import { createMerchantRoutes } from './merchant/routes'
+import { createPosDeviceRoutes } from './merchant/devices/routes'
 
 export function initIocContainer(
   config: typeof Config
@@ -78,11 +79,20 @@ export function initIocContainer(
   container.singleton(
     'posDeviceService',
     async (deps: IocContract<AppServices>) => {
-      const config = await deps.use('config')
       const logger = await deps.use('logger')
       const knex = await deps.use('knex')
-      return await createPosDeviceService({ config, logger, knex })
+      return await createPosDeviceService({
+        logger,
+        knex
+      })
     }
+  )
+
+  container.singleton('posDeviceRoutes', async (deps) =>
+    createPosDeviceRoutes({
+      logger: await deps.use('logger'),
+      posDeviceService: await deps.use('posDeviceService')
+    })
   )
   return container
 }
