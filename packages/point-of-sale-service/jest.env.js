3a0e1ffd3b53816d4c6cb941813940eb9499a909
Max Kurapov <max@interledger.org>
2025-10-22 18:22:28 +0200

    chore: rename point-of-sale to point-of-sale service

--- packages/point-of-sale-service/jest.env.js
+++ packages/point-of-sale-service/jest.env.js
@@ -1,4 +1,4 @@
-// Jest environment configuration for point-of-sale
+// Jest environment configuration for point-of-sale-service
 process.env.NODE_ENV = 'test'
 process.env.LOG_LEVEL = process.env.LOG_LEVEL || 'silent'
 
