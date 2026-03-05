8b77d8d4a21b364b1069366e241202ef71430fdc
sanducb <bogdan.sandu@breakpointit.eu>
2026-03-05 12:33:55 +0200

    chore(backend): use updated pay and stream-receiver from npm, update lockfile

--- packages/pay/jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-module.exports = {
-  collectCoverageFrom: ['src/**/*.ts', '!**/node_modules/**'],
-  coverageReporters: ['text', 'lcov'],
-  coverageDirectory: 'coverage',
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testPathIgnorePatterns: ['/node_modules/', '<rootDir>/dist']
-}
--- packages/stream-receiver/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  collectCoverageFrom: ['src/**/*.ts', '!**/node_modules/**'],
-  coverageReporters: ['text', 'lcov'],
-  coverageDirectory: 'coverage',
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testPathIgnorePatterns: ['/node_modules/', '<rootDir>/dist']
-}
