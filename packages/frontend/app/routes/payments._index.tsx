6a12efe2de451a6d83bb6d9f2b1cb965dd835583
Sid Vishnoi <8426945+sidvishnoi@users.noreply.github.com>
2026-08-06 18:20:52 +0530

    3, 24: add accessible text to filter button in payments page

--- packages/frontend/app/routes/payments._index.tsx
+++ packages/frontend/app/routes/payments._index.tsx
@@ -221,6 +221,7 @@ export default function PaymentsPage() {
                   value={searchParams.get('type') ?? ''}
                 />
                 <Button type='submit'>
+                  <span className='sr-only'>Filter by wallet address ID</span>
                   <svg
                     xmlns='http://www.w3.org/2000/svg'
                     width='16'
@@ -229,6 +230,7 @@ export default function PaymentsPage() {
                     viewBox='0 0 24 24'
                     stroke='currentColor'
                     strokeWidth={2}
+                    aria-hidden='true'
                   >
                     <path
                       strokeLinecap='round'
