0c747e2b95e722712fe6ceceef92c6c32d8b1174
Sid Vishnoi <8426945+sidvishnoi@users.noreply.github.com>
2026-08-12 15:41:52 +0530

    set dismiss timeout to 5s

--- packages/frontend/app/root.tsx
+++ packages/frontend/app/root.tsx
@@ -139,7 +139,7 @@ export default function App() {
             onClose={() => setSnackbarOpen(false)}
             show={snackbarOpen}
             message={message}
-            dismissAfter={2000}
+            dismissAfter={5000}
           />
           <div className='min-h-full relative z-10'>
             {displaySidebar && (
